75e4b8ef21b052f1baee77861d2d2130ba0fb829
John Hanley <jhanley708-git@s6.sector6.net>
2021-10-15 13:10:45 -0700

    Add filter_short_segments()

--- cluster/jutland/dataset.py
+++ cluster/jutland/dataset.py
@@ -1,7 +1,9 @@
 
+from collections import Counter
 from pathlib import Path
 import re
 
+from pandas_profiling import ProfileReport
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -15,10 +17,35 @@ class Dataset:
     @classmethod
     def get_df(cls):
         """Densifies (filters) the somewhat sparse UCI roadway dataset."""
-        cache = Path(re.sub(r'\.txt$', '.parquet', f'{cls.SPATIAL}'))
+        base = re.sub(r'\.txt$', '', f'{cls.SPATIAL}')
+        cache = Path(f'{base}.parquet')
         if not cache.exists():
             cols = 'osm_id lon lat alt'  # Open Street Map ID, deg, deg, meters
             df = pd.read_csv(cls.SPATIAL, names=cols.split())
+            assert (df.alt < 135).all()  # All mentioned roads are near sea level.
+            assert 434874 == len(df), len(df)
+
+            df = cls.filter_short_segments(df)
+            assert 287331 == len(df), len(df)
+
+            cls.profile(df, Path(f'{base}.html'))
             pq.write_table(pa.Table.from_pandas(df), cache)
 
         return pq.read_table(cache).to_pandas()  # Elapsed time is less than two seconds.
+
+    @staticmethod
+    def filter_short_segments(df: pd.DataFrame, k=10):
+        """Demands that a given osm_id shall have at least K segments.
+
+        So e.g. singleton "roads", containing just a single point, are discarded.
+        """
+        counts = Counter(df.osm_id)
+        small_roads = set(osm_id
+                          for osm_id, count in counts.items()
+                          if count < k)
+        return df[~df.osm_id.isin(small_roads)]
+
+    @staticmethod
+    def profile(df: pd.DataFrame, out: Path):
+        if not out.exists():
+            ProfileReport(df).to_file(out)
