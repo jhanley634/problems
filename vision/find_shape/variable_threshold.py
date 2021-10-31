9775f78f0efac30d78c9d35a6854d66f5c6823b6
John Hanley <jhanley708-git@s6.sector6.net>
2021-10-31 12:25:04 -0700

    Allow sweeping the kernel size

--- vision/find_shape/variable_threshold.py
+++ vision/find_shape/variable_threshold.py
@@ -1,14 +1,33 @@
 #! /usr/bin/env streamlit run --server.runOnSave true
 # Copyright 2021 John Hanley. MIT licensed.
+import cv2
 import streamlit as st
 
 from vision.find_shape.find_ngons import urls
+from vision.find_shape.web_image import WebImage
+
+
+@st.cache
+def _read_image(img_idx: int):
+    return cv2.imread(WebImage(urls[img_idx]).image())
 
 
 def main():
-    st.text('Please choose an image:')
-    n = st.slider('image #', 0, len(urls) - 1)
-    st.write(urls[n])
+    img_idx = st.slider('image #', 0, len(urls) - 1)
+    st.write(urls[img_idx])
+
+    ksize = st.slider('kernel_size', 1, 20)
+    sigma = st.slider('sigma', 1, 90)
+
+    # We want an odd number here.
+    if ksize % 2 == 0:
+        ksize += 1
+
+    img = _read_image(img_idx)
+    blur = cv2.GaussianBlur(img, ksize=(ksize, ksize), sigmaX=sigma)
+    st.image(blur)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_OTSU)
 
 
 if __name__ == '__main__':
