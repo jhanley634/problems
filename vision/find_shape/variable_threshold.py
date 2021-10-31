3fb59061b025d9e76dababd15303006c1404c9f0
John Hanley <jhanley708-git@s6.sector6.net>
2021-10-31 12:41:17 -0700

    Display a constant-width (fixed resolution) image

--- vision/find_shape/variable_threshold.py
+++ vision/find_shape/variable_threshold.py
@@ -8,16 +8,21 @@ from vision.find_shape.web_image import WebImage
 
 
 @st.cache
-def _read_image(img_idx: int):
-    return cv2.imread(WebImage(urls[img_idx]).image())
+def _read_image(img_idx: int, new_width=700):
+    img = cv2.imread(WebImage(urls[img_idx]).image())
+    height, width, depth = img.shape
+    assert depth == 3  # color RGB
+    assert width > height
+    new_height = int(height * new_width / width)  # preserve aspect ratio
+    return cv2.resize(img, (new_width, new_height))
 
 
 def main():
     img_idx = st.slider('image #', 0, len(urls) - 1)
     st.write(urls[img_idx])
 
-    ksize = st.slider('kernel_size', 1, 20)
-    sigma = st.slider('sigma', 1, 90)
+    ksize = st.slider('kernel_size', 1, 19, step=2)
+    sigma = st.slider('sigma', 1, 1 + ksize)
 
     # We want an odd number here.
     if ksize % 2 == 0:
@@ -25,8 +30,11 @@ def main():
 
     img = _read_image(img_idx)
     blur = cv2.GaussianBlur(img, ksize=(ksize, ksize), sigmaX=sigma)
-    st.image(blur)
+    disp_img = blur
     gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    if st.checkbox('gray?'):
+        disp_img = gray
+    st.image(disp_img)
     _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_OTSU)
 
 
