310039b74b517d09d0fb0c70cad703bfd439ff88
John Hanley <jhanley708-git@s6.sector6.net>
2022-04-23 13:42:05 -0700

    Add preview.py

---
+++ vision/preview/preview.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env streamlit run --server.runOnSave true
+# Copyright 2022 John Hanley. MIT licensed.
+#
+# example usage:
+#   vision/preview/preview.py -- --pattern=*.jpg
+#
+from glob import glob
+from pathlib import Path
+
+import streamlit as st
+import typer
+
+DESKTOP = Path('~/Desktop').expanduser()
+cli = typer.Typer()
+
+
+@cli.command()
+def preview_images(folder: Path = DESKTOP, pattern='*.png'):
+    assert folder.is_dir(), folder
+    images = [Path(f).name
+              for f in glob(f'{folder}/{pattern}')]
+    if len(images) < 2:
+        images.append('ω')
+    if len(images) < 2:
+        images = ['α'] + images
+    assert len(images) >= 2
+    choice = folder / st.select_slider('file', options=images)
+    if choice.exists():
+        st.image(f'{choice}')
+
+
+if __name__ == '__main__':
+    cli(standalone_mode=False)
