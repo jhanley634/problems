3eea631ccb45b16113631402480eac1bb45db0c3
John Hanley <jhanley708-git@s6.sector6.net>
2022-06-05 15:27:41 -0700

    Add prev_delta adjustment

--- percolate/bin/slider_pair.py
+++ percolate/bin/slider_pair.py
@@ -1,11 +1,22 @@
 #! /usr/bin/env streamlit run --server.runOnSave true
 # Copyright 2022 John Hanley. MIT licensed.
+from sqlite3 import Connection
+
 from uszipcode import SearchEngine
 import pandas as pd
 import streamlit as st
 
+from percolate.bin.slider_pair_state import prev
 
-@st.cache(hash_funcs={SearchEngine: lambda x: str(x)})  # fixes "weakref" diagnostic
+# Hoped to fix a "weakref" diagnostic with this dict.
+# @st.cache(hash_funcs=_hash_funcs)
+_hash_funcs = {
+    Connection: id,
+    SearchEngine: str,
+    # SearchEngine: lambda x: str(x),
+}
+
+
 def _get_search_engine():
     """Returns a zipcode search engine offering "comprehensive" details, connected to sqlite backend.
     """
@@ -19,8 +30,20 @@ def _get_search_engine():
 
 
 def main():
+    prev_lo = prev['lo']
+    prev_delta = min(prev['hi'] - prev['lo'], 50)
+
     lo = st.slider('low', min_value=1, max_value=120)
-    hi = st.slider('high', min_value=1, max_value=120, value=114)
+    widget = st.empty()
+    hi = widget.slider('high', min_value=1, max_value=120, value=prev['hi'])
+
+    if lo != prev_lo:  # We assume mouse will only adjust a single slider per refresh interval.
+        hi = widget.slider('high', min_value=1, max_value=120, value=lo + prev_delta)
+
+    st.write(prev_delta)
+    prev['lo'] = lo
+    prev['hi'] = hi
+
     se = _get_search_engine()
 
     scale = 1000
---
+++ percolate/bin/slider_pair_state.py
@@ -0,0 +1,4 @@
+
+INITIAL = 114
+
+prev = dict(lo=1, hi=INITIAL)
