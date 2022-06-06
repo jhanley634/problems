f26ab7a765159277f5fd2e629b24dc4743cbca67
John Hanley <jhanley708-git@s6.sector6.net>
2022-06-05 22:24:34 -0700

    Use io.TextIOWrapper

--- percolate/bin/subproc_streaming.py
+++ percolate/bin/subproc_streaming.py
@@ -1,28 +1,39 @@
 #! /usr/bin/env python
 from pathlib import Path
 from select import PIPE_BUF, select
-from subprocess import PIPE, Popen, check_output, run
+from subprocess import PIPE, Popen
 import datetime as dt
+import io
 import os
 
 import typer
 
 
+def streaming_subproc(cmd):
+    timeout = dt.timedelta(seconds=5)
+
+    with Popen(cmd, stdout=PIPE) as proc:
+        stdout = io.TextIOWrapper(proc.stdout)
+        fd = stdout.fileno()
+        os.set_blocking(fd, False)
+        buf = 'sentinel'
+        while proc.poll() is None and len(buf) > 0:
+            select([fd], [], [], timeout.total_seconds())
+            buf = stdout.read(PIPE_BUF)
+            if len(buf):
+                yield buf
+        proc.terminate()
+        proc.wait()
+
+
 def parent():
     repo_top = Path(__file__ + '/../../..').resolve()
     os.chdir(repo_top)
-    timeout = dt.timedelta(seconds=5)
-    i = 0
 
     cmd = 'bash percolate/bin/subproc_slow_output.sh 3 1'.split()
-    with Popen(cmd, stdout=PIPE, encoding='utf8') as proc:
-        fd = proc.stdout.fileno()
-        os.set_blocking(fd, False)
-        while proc.poll() is None:
-            select([fd], [], [], timeout.total_seconds())
-            buf = proc.stdout.read(PIPE_BUF)
-            print(f'{i}  {buf} {len(buf)}')
-            i += 1
+    for line in streaming_subproc(cmd):
+        line = line.rstrip('\n')
+        print(f']] {line} [[')
 
 
 if __name__ == '__main__':
