d72874b5a9721216589236aa0b8836ea2c2706d3
John Hanley <jhanley708-git@s6.sector6.net>
2022-06-16 21:43:30 -0700

    Add .rlib target

--- vision/loadtxt.d/rust_lib/src/lib.rs
+++ vision/loadtxt.d/rust_lib/src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::str;
+
+/// Turn a C-string into a string slice and print to console:
+#[no_mangle]
+pub extern "C" fn print_string(c_string_ptr: *const c_char) {
+    let bytes = unsafe { CStr::from_ptr(c_string_ptr).to_bytes() };
+    let str_slice = str::from_utf8(bytes).unwrap();
+    println!("{}", str_slice);
 }
+
+// pub fn main() { eprintln!("hi") }
