f6a04440de9d87de7d398811632b48e665558b86
John Hanley <jhanley708-git@s6.sector6.net>
2022-06-18 17:10:15 -0700

    Add read_number_pairs()

--- vision/loadtxt.d/rust_lib/load_2.py
+++ vision/loadtxt.d/rust_lib/load_2.py
@@ -8,6 +8,9 @@ import typer
 def main(in_folder: Path = '/tmp/loadtxt.d'):
     for f in sorted(in_folder.glob('*.txt')):
         n = load_txt(str(f))
+        print(type(n))
+        print(len(n))
+        print(type(n[0]))
         assert 42 == n, n
     print(f)
 
--- vision/loadtxt.d/rust_lib/src/lib.rs
+++ vision/loadtxt.d/rust_lib/src/lib.rs
@@ -1,21 +1,43 @@
+use std::fs::File;
 use std::io::Read;
 use std::str;
 use pyo3::prelude::*;
+// use pyo3::types::{PyList, PyTuple};
 
 
 #[pyfunction]
-fn load_txt(filename: &str) -> PyResult<f64> {
-    let mut file = std::fs::File::open(filename).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let mut lines = contents.lines();
-    let line = lines.next().unwrap();
-    // let num = line.parse::<f64>().unwrap();
-    // Ok(num)
-    let e = 2.7182818284590452353602874713527;
-    let p = 3.1415926535897932384626433832795;
-    Ok(e + p)
+fn load_txt(path: &str) -> PyResult<Vec<f64>> {
+    let mut infile = File::open(path).unwrap();
+    let mut s = String::new();
+    infile.read_to_string(&mut s).unwrap();
+    let mut v = Vec::new();
+    for line in s.lines() {
+        for field in line.split_whitespace().collect::<Vec<_>>() {
+            v.push(field.parse::<f64>().unwrap());
+        }
+    }
+    Ok(v)
 }
+
+
+#[pyfunction]
+fn read_number_pairs(filename: &str) -> PyResult<f64> {
+    let mut f = std::fs::File::open(filename).unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+    // let mut lines = contents.lines();
+    // for line in lines.iter() {
+    //     let mut words = line.split_whitespace();
+    //     let first = words.next().unwrap();
+    //     let second = words.next().unwrap();
+    //     let first_num = first.parse::<f64>().unwrap();
+    //     let second_num = second.parse::<f64>().unwrap();
+    //     return Ok(first_num + second_num);
+    // }
+    Ok(0.0)
+}
+
+
 
 #[pymodule]
 fn rust_fast(_py: Python, m: &PyModule) -> PyResult<()> {
