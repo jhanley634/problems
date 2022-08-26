3658aefdc5c241290304b62ddcde3f9fa891ae77
John Hanley <jhanley708-git@s6.sector6.net>
2022-08-26 16:08:19 -0700

    Add gen_imports

---
+++ infra/gen_imports.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+# Copyright 2022 John Hanley. MIT licensed.
+#
+# Scans the source tree and summarizes all imports found.
+# This is useful for verifying the conda environment is working properly.
+#
+
+VERIFY=infra/verify_imports.py
+set -e -u
+cd ../problems
+cat  > ${VERIFY}  <<EOF
+#! /usr/bin/env python
+# Copyright 2022 John Hanley. MIT licensed.
+"""
+Verifies that packages load without error, e.g. due to missing deps.
+"""
+# ignore F401 "imported but unused"
+EOF
+
+find . -name '*.py' |
+  sort |
+  xargs egrep -h '^import |^from [^.]' |
+  egrep -v '^from (autoPyTorch |jutland|rust_fast )' |
+  sed -e 's/ as .*//' |
+  awk '{print $0, " # noqa F401"}' |
+  sort -u  >> ${VERIFY}
+
+chmod a+x ${VERIFY}
+isort ${VERIFY}
--- infra/verify_imports.py
+++ infra/verify_imports.py
@@ -1,24 +1,66 @@
 #! /usr/bin/env python
-# Copyright 2021 John Hanley. MIT licensed.
+# Copyright 2022 John Hanley. MIT licensed.
 """
 Verifies that packages load without error, e.g. due to missing deps.
 """
+from collections import Counter  # noqa F401
+from distutils.core import setup  # noqa F401
+from glob import glob  # noqa F401
+from hashlib import blake2b  # noqa F401
+from hashlib import sha3_224  # noqa F401
+from pathlib import Path  # noqa F401
+from pprint import pp  # noqa F401
+from random import seed  # noqa F401
+from select import PIPE_BUF, select  # noqa F401
+from shutil import copyfile  # noqa F401
+from sqlite3 import Connection  # noqa F401
+from subprocess import PIPE, Popen  # noqa F401
+import datetime  # noqa F401
+import io  # noqa F401
+import os  # noqa F401
+import re  # noqa F401
+import subprocess  # noqa F401
+import unittest  # noqa F401
+
+from autoPyTorch import AutoNetClassification  # noqa F401
+from glom import glom  # noqa F401
+from ipyleaflet import AwesomeIcon, Map, Marker, basemaps  # noqa F401
+from jutland.dataset import Dataset  # noqa F401
+from numpy import loadtxt  # noqa F401
+from numpy import nan  # noqa F401
+from numpy.random import default_rng  # noqa F401
+from osmnx.graph import graph_from_place  # noqa F401
+from palettable.colorbrewer import qualitative  # noqa F401
+from pandas_profiling import ProfileReport  # noqa F401
 # ignore F401 "imported but unused"
-from PIL import Image  # noqa F401
-from ruamel.yaml import YAML  # noqa F401
-import autoPyTorch  # noqa F401
-import dask  # noqa F401
-import fasteners  # noqa F401
-import geopandas  # noqa F401
-import geopy  # noqa F401
-import isort  # noqa F401
-import openml  # noqa F401
-import palettable  # noqa F401
-import pandas as pd  # noqa F401
-import psutil  # noqa F401
+from PIL.Image import Image  # noqa F401
+from PIL.ImageDraw import ImageDraw  # noqa F401
+from rust_fast import load_txt  # noqa F401
+from tqdm import tqdm  # noqa F401
+from uszipcode import SearchEngine  # noqa F401
+import click  # noqa F401
+import cv2  # noqa F401
+import fiona  # noqa F401
+import gpxpy  # noqa F401
+import matplotlib.pyplot  # noqa F401
+import networkit  # noqa F401
+import numpy  # noqa F401
+import osmnx  # noqa F401
+import pandas  # noqa F401
 import pyarrow  # noqa F401
+import pyarrow.parquet  # noqa F401
 import pydeck  # noqa F401
-import pynisher  # noqa F401
-import scipy  # noqa F401
-import sklearn  # noqa F401
+import requests  # noqa F401
+import seaborn  # noqa F401
+import sklearn.datasets  # noqa F401
+import sklearn.metrics  # noqa F401
+import sklearn.model_selection  # noqa F401
 import streamlit  # noqa F401
+import typer  # noqa F401
+
+from autoencode.util.projection import _hash_col, feature_subset  # noqa F401
+from percolate.bin.slider_pair_state import prev  # noqa F401
+from percolate.two_d_percolation import Perc  # noqa F401
+from vision.find_shape.find_ngons import BLACK, urls  # noqa F401
+from vision.find_shape.web_image import WebImage  # noqa F401
+from web.wiki.history import HistoryScraper  # noqa F401
