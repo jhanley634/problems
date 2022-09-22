e25debab2360eb9f736bdd77fba99fbfb53397e4
John Hanley <jhanley708-git@s6.sector6.net>
2022-09-22 16:14:50 -0700

    .dropna()

--- feature_sel/ozone/predict.py
+++ feature_sel/ozone/predict.py
@@ -8,6 +8,8 @@ from feature_sel.ozone.ozone import get_df
 def _get_train_test(train_through='1976-06-30'):
     df = get_df()
     assert 366 == len(df)
+    df = df.dropna()
+    assert 203 == len(df)
 
     y_train = df[df.stamp <= train_through].ozone
     y_test = df[df.stamp > train_through].ozone
@@ -22,10 +24,10 @@ def _get_train_test(train_through='1976-06-30'):
             x_test, y_test)
 
 
-def testmain():
+def main():
     x_train, y_train, x_test, y_test = _get_train_test()
-    assert 182 == len(x_train)
-    assert 184 == len(x_test)
+    assert 103 == len(x_train)
+    assert 100 == len(x_test)
     assert len(y_train) == len(x_train)
     assert len(y_test) == len(x_test)
 
