1c8f35b21f1f6531c11a61588f4936b0656421fb
John Hanley <jhanley708-git@s6.sector6.net>
2022-11-17 18:46:10 -0800

    Avoid Path() function call in arg defaults for GenerateSyntheticTextImage

--- autoencode/synth_text/generate_text.py
+++ autoencode/synth_text/generate_text.py
@@ -1,18 +1,18 @@
-
 # Copyright 2022 John Hanley. MIT licensed.
 from pathlib import Path
 
 from PIL.Image import Image
 from PIL.ImageDraw import ImageDraw
 
+_desktop = Path("~/Desktop").expanduser()
+
 
 class GenerateSyntheticTextImage:
-
-    def __init__(self, width=32, height=32, out_dir=Path('~/Desktop')):
+    def __init__(self, width=32, height=32, out_dir=_desktop):
         self.width = width
         self.height = height
         self.out_dir = out_dir.expanduser()
 
-    def generate(self, id_=1001, text='A'):
+    def generate(self, id_=1001, text="A"):
         d = ImageDraw(Image())
-        d.text((0, 0), 'hi')
+        d.text((0, 0), "hi")
