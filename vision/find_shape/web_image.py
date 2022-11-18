9084a93dd2f0984040a104b184b1997fc228d46d
John Hanley <jhanley708-git@s6.sector6.net>
2022-11-17 18:28:48 -0800

    Avoid Path() function call in argument defaults for web_image.py

--- vision/find_shape/web_image.py
+++ vision/find_shape/web_image.py
@@ -1,23 +1,24 @@
-
 # Copyright 2021 John Hanley. MIT licensed.
 from hashlib import sha3_224
 from pathlib import Path
 
 import requests
 
+_tmp = Path("/tmp")
+
 
 class WebImage:
     """Offers cached access to images from the web."""
 
-    def __init__(self, url, fname='shapes.jpg', temp=Path('/tmp')):
+    def __init__(self, url, fname="shapes.jpg", temp=_tmp):
         self.url = url
-        pfx = 'img' + sha3_224(url.encode()).hexdigest()[:4]
-        self.fspec = temp / f'{pfx}_{fname}'
+        pfx = "img" + sha3_224(url.encode()).hexdigest()[:4]
+        self.fspec = temp / f"{pfx}_{fname}"
 
     def image(self):
         if not self.fspec.exists():
             resp = requests.get(self.url)
             resp.raise_for_status()
-            with open(self.fspec, 'wb') as fout:
+            with open(self.fspec, "wb") as fout:
                 fout.write(resp.content)
-        return f'{self.fspec}'
+        return f"{self.fspec}"
