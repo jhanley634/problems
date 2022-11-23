438aa4c14c073aa7e7484c1a0cef55fc8aced73f
John Hanley <jhanley708-git@s6.sector6.net>
2022-11-23 11:51:30 -0800

    Add gen_continuous

--- queuing/caller/gen.py
+++ queuing/caller/gen.py
@@ -10,12 +10,30 @@ class GenerateCallers:
         self, arrival_rate: float = 1 / 30.0  # λ: expect a call every thirty seconds
     ):
         self.arrival_rate = arrival_rate
-        self.q = PriorityQueue()
+        self.q: PriorityQueue = PriorityQueue()
         self.rng = np.random.default_rng()
-        self.id_ = 1  # serial
+        self.id_ = 0  # serial
+
+    def gen_continuous(
+        self,
+        start_time: dt.datetime,
+        end_time: dt.datetime,
+        call_duration_sec: float = 20,
+    ):
+        t = start_time
+        while t < end_time:
+            # advance time to the next event
+            t += dt.timedelta(seconds=self.rng.exponential(1 / self.arrival_rate))
+            dur = dt.timedelta(seconds=call_duration_sec)
+            self.id_ += 1
+            self.q.put((t + dur, self.id_))
+            yield t, dur, self.id_
 
     def gen_discrete(
-        self, start: dt.datetime, end: dt.datetime, call_duration_sec: float = 20
+        self,
+        start: dt.datetime,
+        end: dt.datetime,
+        call_duration_sec: float = 20,
     ):
         one_second = dt.timedelta(seconds=1)
         t = start
@@ -24,16 +42,17 @@ class GenerateCallers:
             num_events = self.rng.poisson(self.arrival_rate)
             for _ in range(num_events):
                 dur = dt.timedelta(seconds=call_duration_sec)
+                self.id_ += 1
                 self.q.put((t + dur, self.id_))
-                yield t, dur
+                yield t, dur, self.id_
 
             t += one_second
 
 
 if __name__ == "__main__":
     gen = GenerateCallers()
-    for stamp, duration in gen.gen_discrete(
+    for stamp, duration, id_ in gen.gen_continuous(
         dt.datetime.now(),
         dt.datetime.now() + dt.timedelta(seconds=1_200),
     ):
-        print(stamp, " ", duration)
+        print(stamp, " ", duration, " ", id_)
