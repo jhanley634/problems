2b0acf18cc5764fb325c677f881be3ff0e68696f
John Hanley <jhanley708-git@s6.sector6.net>
2022-11-26 12:23:24 -0800

    Add tabulate dep

--- geo/directions/directions.py
+++ geo/directions/directions.py
@@ -1,16 +1,24 @@
-#! /usr/bin/env python
+#! /usr/bin/env streamlit run --server.runOnSave true
 # Copyright 2022 John Hanley. MIT licensed.
 from pathlib import Path
 
 import gpxpy
 import pandas as pd
-import typer
+import pydeck as pdk
+import streamlit as st
 
 
-def directions(in_file="/tmp/10-Jul-2022-1714.gpx"):
+def _expect(expected, actual):
+    assert expected == actual, actual
+
+
+def _get_df(in_file="/tmp/10-Jul-2022-1714.gpx"):
     in_file = Path(in_file).expanduser()
     with open(in_file) as fin:
-        _show(gpxpy.parse(fin))
+        gpx = gpxpy.parse(fin)
+        _expect(1, len(gpx.tracks))
+        _expect(1, len(gpx.tracks[0].segments))
+        return pd.DataFrame(_get_points(gpx.tracks[0].segments[0]))
 
 
 def _get_points(segment):
@@ -20,10 +28,30 @@ def _get_points(segment):
         )
 
 
-def _show(gpx):
-    df = pd.DataFrame(_get_points(gpx.tracks[0].segments[0]))
-    print(df)
+def _get_deck(start=(37.46, -122.15)):
+    return pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state=pdk.ViewState(
+            latitude=start[0],
+            longitude=start[1],
+            zoom=9,
+            pitch=30,
+        ),
+        layers=[
+            pdk.Layer(
+                "ColumnLayer",
+                data=_get_df(),
+                get_position="[lon, lat]",
+                get_elevation=90,
+                elevation_scale=1,
+                radius=20,
+                get_fill_color=[10, 10, "[20 * color]"],
+            ),
+        ],
+    )
 
 
 if __name__ == "__main__":
-    typer.run(directions)
+    st.markdown("# route")
+    st.pydeck_chart(_get_deck())
+    st.write(_get_df())
