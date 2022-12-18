51836a131f326fb17df71fe94494a4e1d547e306
John Hanley <jhanley708-git@s6.sector6.net>
2022-12-17 20:29:13 -0800

    Got to a length of 531

--- vision/collatz/collatz_test.py
+++ vision/collatz/collatz_test.py
@@ -1,3 +1,4 @@
+from math import ceil, sqrt
 import unittest
 
 from vision.collatz.collatz import collatz
@@ -34,10 +35,17 @@ class CollatzTest(unittest.TestCase):
         self.assertEqual(collatz(410_011), 449)
         self.assertEqual(collatz(511_935), 470)
         self.assertEqual(collatz(1_000_000), 153)
+        self.assertEqual(collatz(1_252_662), 510)
+        self.assertEqual(collatz(1_564_063), 531)
+        self.assertEqual(collatz(1_590_503), 270)
         self.assertEqual(collatz(10_000_000), 146)
         self.assertEqual(collatz(100_000_000), 108)
         self.assertEqual(collatz(1_000_000_000), 101)
 
     def test_iterate(self):
-        for i in range(1, 500_000):
-            self.assertLess(collatz(i), 450, i)
+        phi = 0.5 * (1 + sqrt(5))
+        n = 2
+        for i in range(1, 410_000):
+            print(" ", i, " ", n, end="       \r")
+            n = 1 + ceil(n * (phi - 0.61803))
+            self.assertLess(collatz(n), 532, (i, n))
