c20c677cefd46ccde97f8223d1bc00a99f0688e0
John Hanley <jhanley708-git@s6.sector6.net>
2022-12-29 21:09:27 -0800

    11 K rows costs about 6 GiB of RAM

--- vision/dist_matrix/distance_matrix.py
+++ vision/dist_matrix/distance_matrix.py
@@ -11,7 +11,7 @@ import seaborn as sns
 
 
 def distance_matrix(df: pd.DataFrame, metric: str = "euclidean"):
-    df = df[["zipcode", "lat", "lng", "population", "population_density"]]
+    df = df[["zipcode", "lat", "lng", "population_density"]]
     df = df.sort_values(by=["zipcode"])
     print(df)
     dist = pdist(df, metric)
@@ -25,7 +25,7 @@ def distance_matrix(df: pd.DataFrame, metric: str = "euclidean"):
 
 def get_df() -> pd.DataFrame:
     search = SearchEngine(SearchEngine.SimpleOrComprehensiveArgEnum.simple)
-    zips = search.by_coordinates(37.46, -122.15, radius=100, returns=10)
+    zips = search.by_population_density(lower=100, returns=11_000)
     df = pd.DataFrame(z.to_dict() for z in zips)
     df = df[
         [
