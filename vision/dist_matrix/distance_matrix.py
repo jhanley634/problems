a075c730a8810445fc53a8a0a4244710b7c2b075
John Hanley <jhanley708-git@s6.sector6.net>
2022-12-29 20:00:53 -0800

    Add get_df()

--- vision/dist_matrix/distance_matrix.py
+++ vision/dist_matrix/distance_matrix.py
@@ -4,21 +4,46 @@
 import gc
 
 from scipy.spatial.distance import pdist, squareform
+from uszipcode import SearchEngine
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
 
-# distance matrix
-def distance_matrix(df_labeled, metric="euclidean"):
-    df_labeled.sort_values(by=["cluster"], inplace=True)
-    dist = pdist(df_labeled, metric)
+def distance_matrix(df: pd.DataFrame, metric: str = "euclidean"):
+    df = df[["zipcode", "lat", "lng", "population", "population_density"]]
+    df = df.sort_values(by=["zipcode"])
+    print(df)
+    dist = pdist(df, metric)
     dist = squareform(dist)
     sns.heatmap(dist, cmap="mako")
     print(dist)
+    plt.show()
     del dist
     gc.collect()
 
 
-final_df = pd.DataFrame([dict(a=1, b=2)])
+def get_df() -> pd.DataFrame:
+    search = SearchEngine(SearchEngine.SimpleOrComprehensiveArgEnum.simple)
+    zips = search.by_coordinates(37.46, -122.15, radius=100, returns=10)
+    df = pd.DataFrame(z.to_dict() for z in zips)
+    df = df[
+        [
+            "zipcode",
+            "post_office_city",
+            "county",
+            "state",
+            "lat",
+            "lng",
+            "population",
+            "population_density",
+            "median_home_value",
+            "median_household_income",
+        ]
+    ]
+    df["zipcode"] = df.zipcode.astype(int)
+    df = df.rename(columns={"post_office_city": "city"})
+    return df
 
-distance_matrix(final_df)
+
+distance_matrix(get_df())
