cf967a579445f64fce78a6630ad133330a7311e4
John Hanley <jhanley708-git@s6.sector6.net>
2023-01-08 12:07:55 -0800

    Add test_all_mods

--- vision/graph_edit/graph_edit.py
+++ vision/graph_edit/graph_edit.py
@@ -12,9 +12,35 @@ class GraphEdit:
         assert a == b, f"Expected square matrix, got {a}x{b}"
         self.edge = edge
         self.edit = {}
+        self._verify_no_self_loops()
+
+    def _verify_no_self_loops(self):
+        for i in range(self.num_nodes):
+            assert self[i, i] == 0, f"self-loop at node {i}"
+
+    @property
+    def num_nodes(self):
+        return len(self.edge)
 
     def __getitem__(self, item):
         return self.edit.get(item, self.edge[item])
 
     def __setitem__(self, item, value):
         self.edit[item] = value
+
+
+def as_array(g: GraphEdit) -> np.ndarray:
+    return np.array([[g[i, j] for j in range(g.num_nodes)] for i in range(g.num_nodes)])
+
+
+def all_mods(g: GraphEdit):
+    """Generates all possible single-edge modifications to the graph."""
+    orig_edit = g.edit.copy()
+    for i in range(g.num_nodes):
+        for j in range(g.num_nodes):
+            if i == j:  # not an edge -- we don't support self-loops
+                continue
+            valid_weights = {0, 1, 2} - {g[i, j]}
+            for w in valid_weights:
+                g.edit = {**orig_edit, (i, j): w}
+                yield g
--- vision/graph_edit/graph_edit_test.py
+++ vision/graph_edit/graph_edit_test.py
@@ -3,14 +3,15 @@
 # https://stackoverflow.com/questions/75038373/find-all-possible-modifications-to-a-graph
 import unittest
 
+from numpy.testing import assert_array_equal
 import numpy as np
 
-from vision.graph_edit.graph_edit import GraphEdit
+from vision.graph_edit.graph_edit import GraphEdit, all_mods, as_array
 
 
 class GraphEditTest(unittest.TestCase):
-    def test_graph_edit(self):
-        g = GraphEdit(
+    def setUp(self):
+        self.g = GraphEdit(
             np.array(
                 [
                     [0, 2, 1, 2, 0],
@@ -22,10 +23,33 @@ class GraphEditTest(unittest.TestCase):
                 dtype=np.uint8,
             )
         )
+
+    def test_graph_edit(self):
+        g = self.g
+        self.assertEqual(5, self.g.num_nodes)
         self.assertEqual(2, g[0, 1])
         g[0, 1] = 3
         self.assertEqual(3, g[0, 1])
         del g.edit[(0, 1)]
         self.assertEqual(2, g[0, 1])
 
-        print(g.edge.dtype)
+    def test_non_square(self):
+        with self.assertRaises(AssertionError):
+            GraphEdit(np.array([[0, 0], [1, 1], [2, 2]]))
+
+    def test_all_mods(self):
+        g = GraphEdit(np.array([[0, 0], [1, 0]]))
+        assert_array_equal(
+            np.array([[0, 0], [1, 0]]),
+            as_array(g),
+        )
+
+        expected = [
+            np.array([[0, 1], [1, 0]]),
+            np.array([[0, 2], [1, 0]]),
+            np.array([[0, 0], [0, 0]]),
+            np.array([[0, 0], [2, 0]]),
+        ]
+
+        for ex, actual in zip(expected, map(as_array, all_mods(g))):
+            assert_array_equal(ex, actual)
