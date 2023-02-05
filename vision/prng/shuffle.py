0ab297a1f303372834611c1dc71758b7ec757ceb
John Hanley <jhanley708-git@s6.sector6.net>
2023-02-05 12:42:38 -0800

    Add plot_randint_counts

--- vision/prng/shuffle.py
+++ vision/prng/shuffle.py
@@ -3,9 +3,11 @@
 # Copyright 2023 John Hanley. MIT licensed.
 
 # https://stackoverflow.com/questions/75348436/get-an-evenly-distributed-subset-of-combinations-without-repetition
+# cf https://math.stackexchange.com/questions/2245194/what-is-the-standard-deviation-of-dice-rolling
 from pprint import pp
 import random
 
+from numpy.random import default_rng
 import matplotlib.pyplot as plt
 import pandas as pd
 import typer
@@ -29,23 +31,52 @@ class Options:
             yield "".join(self.choose_options())
 
 
-def main(n: int = 1_000_000):
+def main(n: int = 1_000_000, lo: int = 0, hi: int = 1000, shuffle: bool = False):
     opt = Options(list("ABCDEFGH"))
     demo = list(opt.choose_many_options(3))
     pp(demo, width=22)
 
-    df = pd.DataFrame(opt.choose_many_options(n), columns=["opt"])
+    rng = default_rng()
+    plot_randint_counts(pd.DataFrame(rng.integers(lo, hi, size=n), columns=["val"]))
+
+    if shuffle:
+        plot_shuffle_combination_counts(
+            pd.DataFrame(opt.choose_many_options(n), columns=["opt"])
+        )
+
+    plt.show()
+
+
+def plot_randint_counts(df, verbose=False):
     df["cnt"] = 1
-    with pd.option_context("display.min_rows", 16):
-        print(df.groupby("opt").sum())
-    cnts = df.groupby("opt").sum().cnt.tolist()
-    plt.plot(range(len(cnts)), cnts)
-    plt.plot(range(len(cnts)), sorted(cnts))
+    df_summary = df.groupby("val").sum()
+    if verbose:
+        with pd.option_context("display.min_rows", 16):
+            print(df_summary)
+    counts = df_summary.cnt.tolist()
+    plt.plot(range(len(counts)), counts)
+    plt.plot(range(len(counts)), sorted(counts))
+    plt.gca().set_ylim((0, None))
+    plt.gca().set_xlabel("rolled dice value")
+    plt.gca().set_ylabel("number of occurrences")
+
+    print(df_summary.query("cnt < 920"))
+    print(df_summary.query("cnt > 1080"))
+
+    print("mean:", round(df.val.mean(), 3), "std:", round(df.val.std(), 3))
+    with pd.option_context("display.float_format", "{:.3f}".format):
+        print(df_summary.describe())
+
+
+def plot_shuffle_combination_counts(df):
+    df["cnt"] = 1
+    counts = df.groupby("opt").sum().cnt.tolist()
+    plt.plot(range(len(counts)), counts)
+    plt.plot(range(len(counts)), sorted(counts))
     plt.gca().set_xlim((0, 1700))
     plt.gca().set_ylim((0, None))
     plt.gca().set_xlabel("combination of options")
     plt.gca().set_ylabel("number of occurrences")
-    plt.show()
 
 
 if __name__ == "__main__":
