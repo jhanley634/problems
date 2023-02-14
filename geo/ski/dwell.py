ef43036f42c1af3d1a06adb18e21647c5bdfbc2c
John Hanley <jhanley708-git@s6.sector6.net>
2023-02-13 21:16:10 -0800

    Add speed column

--- geo/ski/dwell.py
+++ geo/ski/dwell.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from typing import Optional
 import datetime as dt
 
+from geopy import Point
+from geopy.distance import great_circle
 from gpxpy.gpx import GPX
 import gpxpy
 import polars as pl
@@ -20,17 +22,29 @@ def main(infile="~/Desktop/gpx/2022-07-14-1234-pizza.gpx"):
 
 
 def get_breadcrumbs(gpx: GPX, precision=6):
-    prev: Optional[dt.datetime] = None
+    prev_time: Optional[dt.datetime] = None
+    prev_loc: Optional[Point] = None
     for t, track in enumerate(gpx.tracks):
         for s, segment in enumerate(track.segments):
             for point in segment.points:
-                prev = prev or point.time
-                delta = point.time - prev  # type: ignore
+                prev_time = prev_time or point.time
+                delta_t = point.time - prev_time  # type: ignore
+                elapsed = max(0.001, delta_t.total_seconds())
                 lat = round(point.latitude, precision)
                 lng = round(point.longitude, precision)
-                print(t, s, delta, point.time, f"   {lat:.6f}  {lng:.6f}")
-                yield dict(stamp=point.time, lat=lat, lng=lng, delta=delta)
-                prev = point.time
+                loc = Point(lat, lng)
+                delta_x = great_circle(prev_loc, loc).meters if prev_loc else 0
+                print(t, s, delta_t, point.time, f"   {lat:.6f}  {lng:.6f}")
+                yield dict(
+                    stamp=point.time,
+                    lat=lat,
+                    lng=lng,
+                    delta_t=delta_t,
+                    delta_x=delta_x,
+                    speed=delta_x / elapsed,
+                )
+                prev_time = point.time
+                prev_loc = loc
 
 
 if __name__ == "__main__":
