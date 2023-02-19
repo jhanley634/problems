c2207f274de4765f1e5b4f866ffd73831c4e19a6
John Hanley <jhanley708-git@s6.sector6.net>
2023-02-19 15:30:55 -0800

    Add _create_graph()

--- geo/ski/word_ladder.py
+++ geo/ski/word_ladder.py
@@ -2,7 +2,11 @@
 from collections import namedtuple
 import re
 
+from networkx import Graph
 from sortedcontainers import SortedList
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 class Node(namedtuple("Node", "prefix, suffix, word")):
@@ -13,17 +17,55 @@ class WordLadder:
     def __init__(self, length: int = 3, words_file="/usr/share/dict/words"):
         letters_re = re.compile(r"^[a-zA-Z]+$")
         with open(words_file) as fin:
-            self.words = set(
+            self.words = {
                 word.lower()
                 for word in fin.read().splitlines()
                 if len(word) == length and letters_re.match(word)
-            )
+            }
         self.nodes = SortedList()
         for word in self.words:
             for i in range(len(word)):
                 prefix = word[:i]
                 suffix = word[i + 1 :]
-                self.nodes.add(Node(prefix, suffix, word))
+                node = Node(prefix, suffix, word)
+                self.nodes.add(node)
+        self.g = self._create_graph()
+
+    def _create_graph(self):
+        g = Graph()
+        for node in tqdm(self.nodes):
+            g.add_node(node.word)
+            i = self.nodes.bisect_left(node)
+            assert self.nodes[i] == node
+            while i < len(self.nodes) and self.nodes[i].prefix == node.prefix:
+                other = self.nodes[i]
+                i += 1
+                if other.word == node.word:
+                    continue
+                if other.prefix == node.prefix and other.suffix == node.suffix:
+                    g.add_node(other.word)
+                    g.add_edge(other.word, node.word)
+        return g
+
+    def find_path(self, start: str, end: str):
+        p = nx.shortest_path(self.g, start, end)
+        assert isinstance(p, list)
+        return p
+
+    def display_graph(self):
+        self._display(self.g)
+
+    def display_spanning_tree(self):
+        self._display(nx.maximum_spanning_tree(self.g))
+
+    @staticmethod
+    def _display(graph):
+        pos = nx.spring_layout(graph)
+        nx.draw_networkx_nodes(graph, pos)
+        nx.draw_networkx_edges(graph, pos, edge_color="grey", alpha=0.5)
+        nx.draw_networkx_labels(graph, pos, font_size=16, font_family="sans-serif")
+        plt.axis("off")
+        plt.show()
 
 
 if __name__ == "__main__":
--- geo/ski/word_ladder_test.py
+++ geo/ski/word_ladder_test.py
@@ -1,17 +1,49 @@
 import unittest
 
+from sortedcontainers import SortedList
+
 from .word_ladder import WordLadder
+
+
+class TestSortedList(unittest.TestCase):
+    def test_sorted_list(self):
+        xs = SortedList([9, 5, 6, 7])
+        self.assertEqual([5, 6, 7, 9], xs)
+        self.assertEqual(6, xs[1])
+        self.assertEqual([5, 6], xs[:2])
+
+        i = xs.bisect_left(4)
+        self.assertEqual(0, i)
+        i = xs.bisect_left(5)
+        self.assertEqual(0, i)
+        i = xs.bisect_left(7)
+        self.assertEqual(2, i)
+
+        i = xs.bisect_right(7)
+        self.assertEqual(3, i)
+        i = xs.bisect_right(9)
+        self.assertEqual(4, i)
+        i = xs.bisect_right(10)
+        self.assertEqual(4, i)
 
 
 class TestWordLadder(unittest.TestCase):
     def test_init(self):
         for length, num_words in [
             (17, 1813),
-            (10, 30_824),
-            (4, 4994),
+            # (10, 30_824),
+            # (4, 4994),
             (3, 1294),
         ]:
             wl = WordLadder(length=length)
             self.assertEqual(num_words, len(wl.words))
 
         self.assertEqual(3882, len(wl.nodes))
+
+        # Pick an arbitrary word.
+        i = int(len(wl.nodes) * 0.1)
+        self.assertEqual("pen", wl.nodes[i].word)
+
+        self.assertEqual(["dog", "cog", "cag", "cat"], wl.find_path("dog", "cat"))
+        self.assertEqual(["cat", "cag", "cog", "dog"], wl.find_path("cat", "dog"))
+        self.assertEqual(["cat", "caw", "cow", "pow"], wl.find_path("cat", "pow"))
