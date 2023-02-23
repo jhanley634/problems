327d469e54f7ebc86ef9797a7c9f057dfd23ae0a
John Hanley <jhanley708-git@s6.sector6.net>
2023-02-23 07:44:43 -0800

    Enforce .isalpha()

--- geo/ski/word_ladder.py
+++ geo/ski/word_ladder.py
@@ -10,42 +10,43 @@ def hamming_distance(a: str, b: str) -> int:
 
 
 class Word(str):
-    ...
+    def __init__(self, word: str):
+        assert word.isalpha(), word
+        super().__init__()
 
 
 class WordLadder:
     def __init__(self, length: int = 3, words_file="/usr/share/dict/words"):
-        self.words: defaultdict[str, Set[str]] = defaultdict(set)
+        self.words: defaultdict[str, Set[Word]] = defaultdict(set)
         with open(words_file) as fin:
             for prototype, word in sorted(self._read_words(length, fin)):
                 assert "_" in prototype
-                assert word.isalpha()
                 self.words[prototype].add(word)
 
     @classmethod
     def _read_words(
         cls, length: int, fin: TextIO
     ) -> Generator[Tuple[str, Word], None, None]:
-        for word in fin:
-            word = Word(word).lower().rstrip()
-            if len(word) == length and word.isalpha():
+
+        for line in map(str.rstrip, fin):
+            if len(line) == length and line.isalpha():
+                word = Word(line.lower())
                 for prototype in cls._gen_prototypes(word):
-                    yield prototype, Word(word)
+                    yield prototype, word
 
     @staticmethod
-    def _gen_prototypes(word: str) -> Generator[str, None, None]:
+    def _gen_prototypes(word: Word) -> Generator[str, None, None]:
 
-        assert word.isalpha()
         for i in range(len(word)):
             prefix = word[:i]
             suffix = word[i + 1 :]
             yield f"{prefix}_{suffix}"
 
-    def find_path(self, start: str, end: str) -> List[str]:
+    def find_path(self, start: Word, end: Word) -> List[Word]:
         paths = sorted(self.bfs_paths(start, end), key=len) + [[]]
         return paths[0]
 
-    def bfs_paths(self, start: str, end: str) -> Generator[List[str], None, None]:
+    def bfs_paths(self, start: Word, end: Word) -> Generator[List[Word], None, None]:
         """Generates candidate acyclic paths from start to end."""
         assert start.isalpha()
         assert end.isalpha()
@@ -63,6 +64,6 @@ class WordLadder:
                         queue.append((word, path + [word]))
 
     @staticmethod
-    def _ordered(neighbors: Set[str], end: str) -> List[str]:
+    def _ordered(neighbors: Set[Word], end: Word) -> List[Word]:
         distance_to_goal = partial(hamming_distance, end)
         return sorted(sorted(neighbors), key=distance_to_goal)
