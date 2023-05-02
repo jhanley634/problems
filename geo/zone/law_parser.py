7d1b534e19bdee109a61e6ee6caa908893b9ecb9
John Hanley <jhanley708-git@s6.sector6.net>
2023-05-02 10:28:30 -0700

    Add get_paragraphs()

--- geo/zone/law_parser.py
+++ geo/zone/law_parser.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
+# Copyright 2023 John Hanley. MIT licensed.
 from pathlib import Path
+from typing import Generator
 
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 import typer
 
 
@@ -13,18 +16,23 @@ class LawParser:
         assert path.exists()
         assert path.suffix == ".html"
         self.markdown: Path = path.with_suffix(".md")
-        self.soup = BeautifulSoup(path.read_text(), features="html.parser")
+        self.soup = BeautifulSoup(path.read_text(), "html.parser")
 
-    def parse(self) -> None:
-        soup = self.soup.find_all("html")
-        print(len(soup), type(soup))
-        for i, tag in enumerate(soup):
-            with open(f"/tmp/{i}.html", "w") as fout:
-                fout.write(str(tag))
+    def parse(self) -> "LawParser":  # builder pattern
+        tag = self.soup.find_all("html")[-1]  # There's six(!?!) of them.
+        # We _do_ see indent hints, e.g. the .5 in:  <p style="...;margin-left: 2.5em;">
+        with open("/tmp/out.html", "w") as fout:
+            fout.write(tag.prettify())
+        self.soup = BeautifulSoup(tag.prettify(), "html.parser")
+        return self
+
+    def get_paragraphs(self) -> Generator[Tag, None, None]:
+        yield from self.soup.find_all("p")
 
 
 def main(input_html_file: Path) -> None:
     LawParser(input_html_file).parse()
 
 
-typer.run(main)
+if __name__ == "__main__":
+    typer.run(main)
---
+++ geo/zone/law_parser_test.py
@@ -0,0 +1,13 @@
+# Copyright 2023 John Hanley. MIT licensed.
+from pathlib import Path
+import unittest
+
+from geo.zone.law_parser import LawParser
+
+
+class TestLawParser(unittest.TestCase):
+    def test_parse(self) -> None:
+        desktop = Path("~/Desktop").expanduser()
+        in_file = desktop / "GOV_65852.2.html"
+        paragraphs = list(LawParser(in_file).parse().get_paragraphs())
+        self.assertEqual(111, len(paragraphs))
