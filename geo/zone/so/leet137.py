d538aa156cde8dbe476eb31ac602ef155b3e73f5
John Hanley <jhanley708-git@s6.sector6.net>
2023-07-04 15:37:16 -0700

    Add test_negative_binary

--- geo/zone/so/leet137.py
+++ geo/zone/so/leet137.py
@@ -1,10 +1,9 @@
-#! /usr/bin/env python
 # Copyright 2023 John Hanley. MIT licensed.
 # from https://codereview.stackexchange.com/questions/285911/leetcode-137-single-number-ii-python-bit-operations
-from typing import List
+import unittest
 
 
-def singleNumber(self, nums: List[int]) -> int:
+def single_number(nums: list[int]) -> int:
     INT_BASE = 33  # because of INT32_MIN
     # give python what it likes
     counts_nz = [0 for _ in range(INT_BASE)]
@@ -26,3 +25,13 @@ def singleNumber(self, nums: List[int]) -> int:
         )
 
     return int(bin_res, 2)
+
+
+class TestLeet137(unittest.TestCase):
+    def test_leet137(self):
+        self.assertEqual(3, single_number([2, 2, 3, 2]))
+        self.assertEqual(99, single_number([0, 1, 0, 1, 0, 1, 99]))
+
+    def test_negative_binary(self):
+        num = -6
+        self.assertEqual("-00000000000000000000000000000110", f"{num:033b}")
