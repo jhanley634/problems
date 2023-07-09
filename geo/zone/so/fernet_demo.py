644032c56b15fd71f07eccc176634c4036d37427
John Hanley <jhanley708-git@s6.sector6.net>
2023-07-09 12:18:36 -0700

    Lint

--- geo/zone/so/fernet_demo.py
+++ geo/zone/so/fernet_demo.py
@@ -5,11 +5,10 @@
 import json
 
 from cryptography.fernet import Fernet
-from pypasswords import *
+from pypasswords import hash_it, match_it
 
 
 def base_generator(password: str):
-
     """
     Generates key and salt with password and then rehashes to make a derived key.
     This derived key is then saved in a dictionary with the salt for further security
@@ -20,18 +19,12 @@ def base_generator(password: str):
 
     key, salt = hash_it(password, salting=True)
     derived_key = hash_it(key)
-    json_save = {
-        'main': {
-            'derived_key': derived_key,
-            'salt': salt
-        }
-    }
+    json_save = {'main': {'derived_key': derived_key, 'salt': salt}}
 
     return json_save
 
 
 def re_hash(json_save: dict):
-
     """
     Re-encrypts the values with the key for further security if any at all as the key is still saved
     in the dictionary.
@@ -40,15 +33,18 @@ def re_hash(json_save: dict):
     """
 
     key = Fernet.generate_key()
-    json_save['main']['derived_key'] = Fernet(key).encrypt(json_save['main']['derived_key'].encode()).decode()
-    json_save['main']['salt'] = Fernet(key).encrypt(json_save['main']['salt'].encode()).decode()
+    json_save['main']['derived_key'] = (
+        Fernet(key).encrypt(json_save['main']['derived_key'].encode()).decode()
+    )
+    json_save['main']['salt'] = (
+        Fernet(key).encrypt(json_save['main']['salt'].encode()).decode()
+    )
     json_save['main']['key'] = key.decode()
 
     return json_save
 
 
 def password_hasher(password: str):
-
     """
     Combination of 'base_generator' and 're_hash'
     """
@@ -60,7 +56,6 @@ def password_hasher(password: str):
 
 
 def password_checker(json_save, password):
-
     """
     Basically undoes all the hashing and checks if password and salt equal to derived key
     """
@@ -68,7 +63,9 @@ def password_checker(json_save, password):
     # Converting rehashed values back to originals
     key = json_save['main']['key'].encode()
     salt = Fernet(key).decrypt(json_save['main']['salt'].encode()).decode()
-    derived_key = Fernet(key).decrypt(json_save['main']['derived_key'].encode()).decode()
+    derived_key = (
+        Fernet(key).decrypt(json_save['main']['derived_key'].encode()).decode()
+    )
 
     # Preparing for password confirmation
     password_key, salt = hash_it(password, salting=True, static_salt=salt)
@@ -78,7 +75,6 @@ def password_checker(json_save, password):
 
 
 def json_file(command, json_data):
-
     if command in ['dump', 'load']:
         if command == 'dump':
             with open('data.json', 'w') as data_file:
