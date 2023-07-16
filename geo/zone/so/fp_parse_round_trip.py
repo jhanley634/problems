aaf2556b8e0a115fc3b94c8e487ab805cce2dbfe
John Hanley <jhanley708-git@s6.sector6.net>
2023-07-15 19:08:38 -0700

    Add some cases that are near 7/8

--- geo/zone/so/fp_parse_round_trip.py
+++ geo/zone/so/fp_parse_round_trip.py
@@ -13,8 +13,11 @@ def to_fp(num: str) -> float:
         num += "."
     i = num.index(".")
 
-    # return acc + _to_int(n[i + 1 :]) / 10 ** (len(n) - i - 1)
-    # The above return is correct, but let's do it the hard way, digit-by-digit.
+    # acc = abs(_to_int(num[:i])) + _to_int(num[i + 1 :]) / 10 ** (len(num) - i - 1)
+    # if num.startswith("-"):
+    #     acc *= -1
+    # return acc
+    # The above is correct, but let's do it the hard way, digit-by-digit.
 
     acc = 0
     exp = 10 ** (len(num) - i - 1)
@@ -68,7 +71,6 @@ class TestFP(unittest.TestCase):
             self.assertEqual(float(n), to_fp(n))
 
     def test_roundoff_issues(self):
-        # Sigh!
         self.assertEqual(0.1, to_fp(".1"))
         self.assertEqual(0.2, to_fp(".2"))
         self.assertEqual(0.3, to_fp(".3"))
@@ -76,13 +78,18 @@ class TestFP(unittest.TestCase):
         self.assertEqual(0.30000000000000004, 3 * 0.1)
         self.assertEqual(0.4, to_fp(".4"))
         self.assertEqual(0.5, to_fp(".5"))
-        self.assertEqual(0.6000000000000001, to_fp(".6"))
+        self.assertEqual(0.6, to_fp(".6"))
         self.assertEqual(0.7, 7 / 10)
         self.assertEqual(0.7000000000000001, 7 * 0.1)
         self.assertEqual(0.7000000000000001, 7 * 10**-1)
-        self.assertEqual(0.7000000000000001, to_fp(".7"))
-        self.assertEqual(0.7500000000000001, to_fp(".75"))
-        self.assertEqual(0.7509765625000001, to_fp("0.7509765625"))
+        self.assertEqual(0.7, to_fp(".7"))
+        self.assertEqual(0.75, to_fp(".75"))
+        self.assertEqual(0.7509765625, to_fp("0.7509765625"))
+        self.assertEqual(0.8750000000582077, to_fp("0.8750000000582076"))
+        self.assertEqual(0.8750000000582078, to_fp("0.8750000000582077"))
+        self.assertEqual(0.8750000000582079, to_fp("0.8750000000582078"))
+        self.assertEqual(0.8750000000582080, to_fp("0.8750000000582079"))
+        self.assertEqual("0x1.c000000080000p-1", (0.8750000000582077).hex())
 
     def test_hex_representation(self):
         self.assertEqual("0x1.999999999999ap-4", (0.1).hex())
@@ -96,6 +103,8 @@ class TestFP(unittest.TestCase):
         self.assertEqual("0x1.ccccccccccccdp-1", (0.9).hex())
         self.assertEqual("0x1.0000000000000p+0", (1.0).hex())
 
+        self.assertEqual("0x1.0000000000000p-2", (0.25).hex())
+
     @given(st.floats(allow_infinity=False, allow_nan=False))
     def test_torture(self, f: float):
         num = str(f)
@@ -103,5 +112,4 @@ class TestFP(unittest.TestCase):
             if "+" in num or num.rindex("-") > 0:
                 return  # We don't support parsing e.g. "1e+16" or "1e-05"
 
-        return
         assert float(num) == to_fp(num)
