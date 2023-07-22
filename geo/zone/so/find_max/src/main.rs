3029fc4dfbf2ac0ba23f7d311c28be882b724af9
John Hanley <jhanley708-git@s6.sector6.net>
2023-07-22 16:01:47 -0700

    Add roll_randoms

--- geo/zone/so/find_max/src/main.rs
+++ geo/zone/so/find_max/src/main.rs
@@ -2,19 +2,27 @@
 
 use rand::Rng;
 
+const N: usize = 100_000;
+
 fn main() {
-    find_max()
+    let xs = roll_randoms();
+    find_max(xs);
 }
 
-fn find_max() {
-    let xs = [0.2, 1., 0.3];
+fn find_max(xs: [f32; N]) {
     let max = xs
         .into_iter()
         .max_by(|a, b| a.partial_cmp(b).unwrap())
         .unwrap();
     // let integer_max = xs.iter().copied().max().unwrap();
-    println!("x {} y {:?} z", max, xs);
+    println!("x {} y", max);
 
-    let num = rand::thread_rng().gen_range(0..100);
-    println!("{}", num);
+    let num = rand::thread_rng().gen_range(0.1f32..0.3f32);
+
+    println!("{}\t{}", num, num.sqrt());
+}
+
+fn roll_randoms() -> [f32; N] {
+    let xs: [f32; N] = [(); N].map(|_| rand::thread_rng().gen_range(0.0..1.0));
+    return xs;
 }
