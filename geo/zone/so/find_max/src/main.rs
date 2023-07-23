c00411da52a9cdd2e120b7ab5fa3cebcbc7ad521
John Hanley <jhanley708-git@s6.sector6.net>
2023-07-22 19:45:43 -0700

    Add sorted()

--- geo/zone/so/find_max/src/main.rs
+++ geo/zone/so/find_max/src/main.rs
@@ -1,21 +1,38 @@
 // Copyright 2023 John Hanley. MIT licensed.
 
+// use ordered_float::OrderedFloat;
+// use std::f32::NAN;
 use rand::Rng;
+use std::time::Instant;
 
 const N: usize = 100_000;
 
 fn main() {
-    let xs = roll_randoms();
+    let xs = sorted(roll_randoms());
     find_max(xs);
 }
 
+fn sorted(mut xs: [f32; N]) -> [f32; N] {
+    // let mut xs = vec![xs_in];
+    // xs_in.clone();
+    xs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    xs
+}
+
+// fn convert(xs: Vec<[f32; N]>) -> Vec<OrderedFloat<[f32; N]>> {
+//     xs as Vec<OrderedFloat<[f32; N]>>
+// }
+
 fn find_max(xs: [f32; N]) {
+    let t0 = Instant::now();
     let max = xs
         .into_iter()
         .max_by(|a, b| a.partial_cmp(b).unwrap())
         .unwrap();
     // let integer_max = xs.iter().copied().max().unwrap();
-    println!("x {} y", max);
+    let elapsed = t0.elapsed();
+
+    println!("found {} in {:.6} seconds", max, elapsed.as_secs_f64());
 
     let num = rand::thread_rng().gen_range(0.1f32..0.3f32);
 
