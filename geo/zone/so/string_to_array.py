16111f335189cf87665e32ff332db7faf05b9092
John Hanley <jhanley708-git@s6.sector6.net>
2023-08-04 14:50:27 -0700

    Strip the BoM

--- geo/zone/so/string_to_array.py
+++ geo/zone/so/string_to_array.py
@@ -5,21 +5,23 @@ from io import BytesIO
 import numpy as np
 
 
-def _get_codec(s: str) -> str:
+def _get_codec(s: str) -> (int, str):
+    """Returns (bom_bytes_to_strip, codec)"""
     if s == "" or (max_val := max(map(ord, s))) < 128:
-        return "ascii"
+        return (0, "ascii")
 
     match 1 + max_val.bit_length() // 8:
         case 1:
-            return "latin-1"
+            return 0, "latin-1"
         case 2:
-            return "utf-16"
+            return 2, "utf-16"
         case 3 | 4:
-            return "utf-32"
+            return 4, "utf-32"
         case _:
             raise ValueError(f"max_val {max_val} not supported")
 
 
 def string_to_array(s: str) -> np.ndarray:
-    a = np.frombuffer(BytesIO(s.encode(_get_codec(s))).getbuffer(), dtype=np.uint8)
-    return a
+    strip, codec = _get_codec(s)
+    buf = BytesIO(s.encode(codec)).getbuffer()
+    return np.frombuffer(buf[strip:], dtype=np.uint8)
--- geo/zone/so/string_to_array_test.py
+++ geo/zone/so/string_to_array_test.py
@@ -2,34 +2,36 @@
 
 import unittest
 
-import numpy as np
-
 from geo.zone.so.string_to_array import _get_codec, string_to_array
 
 
 class StringToArrayTest(unittest.TestCase):
     def test_get_codec(self):
-        self.assertEqual("ascii", _get_codec(""))
-        self.assertEqual("ascii", _get_codec("hi"))
-        self.assertEqual("utf-16", _get_codec("hi χ"))
-        self.assertEqual("utf-16", _get_codec("hi 气"))
-        self.assertEqual("utf-32", _get_codec("hi 🌱"))
+        self.assertEqual((0, "ascii"), _get_codec(""))
+        self.assertEqual((0, "ascii"), _get_codec("hi"))
+        self.assertEqual((2, "utf-16"), _get_codec("hi χ"))
+        self.assertEqual((2, "utf-16"), _get_codec("hi 气"))
+        self.assertEqual((4, "utf-32"), _get_codec("hi 🌱"))
 
     def test_string_to_array(self):
-        # s = "hi 气 ."  # 0x6c14; 🌱 0x1f331
-        s = "hi χ ."  # 0x3c7
-        s = "hi   ."
-        self.assertEqual(105, max(map(ord, s)))
-
-        self.assertEqual(np.uint8, string_to_array(s).dtype)
-        self.assertEqual(6, len(string_to_array(s)))
+        s = "hi"
         self.assertEqual(
-            # "ÿþh\x00i\x00 \x00Ç\x03 \x00.\x00",
-            "hi   .",
-            "".join(map(chr, string_to_array(s))),
-        )
-        self.assertEqual(
-            # [255, 254, 104, 0, 105, 0, 32, 0, 199, 3, 32, 0, 46, 0],
-            [104, 105, 32, 32, 32, 46],
+            [104, 105],
             string_to_array(s).tolist(),
         )
+        s = "hi χ 气"
+        self.assertEqual(
+            [104, 0, 105, 0, 32, 0, 199, 3, 32, 0, 20, 108],
+            string_to_array(s).tolist(),
+        )
+        s = "hi 气 🌱"
+        self.assertEqual(
+            [104, 0, 0, 0, 105, 0, 0, 0, 32, 0, 0, 0]
+            + [20, 108, 0, 0, 32, 0, 0, 0, 49, 243, 1, 0],
+            string_to_array(s).tolist(),
+        )
+
+    def test_roundtrip(self):
+        s = "hi χ 气 🌱"
+        _, codec = _get_codec(s)
+        self.assertEqual(s, string_to_array(s).tobytes().decode(codec))
