85ce1743c5e8880cc88fc0f70ca362df01a1cbec
John Hanley <jhanley708-git@s6.sector6.net>
2023-08-04 14:36:02 -0700

    Add _get_codec

--- geo/zone/so/string_to_array.py
+++ geo/zone/so/string_to_array.py
@@ -5,6 +5,21 @@ from io import BytesIO
 import numpy as np
 
 
+def _get_codec(s: str) -> str:
+    if s == "" or (max_val := max(map(ord, s))) < 128:
+        return "ascii"
+
+    match 1 + max_val.bit_length() // 8:
+        case 1:
+            return "latin-1"
+        case 2:
+            return "utf-16"
+        case 3 | 4:
+            return "utf-32"
+        case _:
+            raise ValueError(f"max_val {max_val} not supported")
+
+
 def string_to_array(s: str) -> np.ndarray:
-    a = np.frombuffer(BytesIO(s.encode("utf-16")).getbuffer(), dtype=np.uint8)
+    a = np.frombuffer(BytesIO(s.encode(_get_codec(s))).getbuffer(), dtype=np.uint8)
     return a
--- geo/zone/so/string_to_array_test.py
+++ geo/zone/so/string_to_array_test.py
@@ -4,20 +4,32 @@ import unittest
 
 import numpy as np
 
-from geo.zone.so.string_to_array import string_to_array
+from geo.zone.so.string_to_array import _get_codec, string_to_array
 
 
 class StringToArrayTest(unittest.TestCase):
+    def test_get_codec(self):
+        self.assertEqual("ascii", _get_codec(""))
+        self.assertEqual("ascii", _get_codec("hi"))
+        self.assertEqual("utf-16", _get_codec("hi χ"))
+        self.assertEqual("utf-16", _get_codec("hi 气"))
+        self.assertEqual("utf-32", _get_codec("hi 🌱"))
+
     def test_string_to_array(self):
-        # s = "hi 气 ."
-        s = "hi χ ."
+        # s = "hi 气 ."  # 0x6c14; 🌱 0x1f331
+        s = "hi χ ."  # 0x3c7
+        s = "hi   ."
+        self.assertEqual(105, max(map(ord, s)))
+
         self.assertEqual(np.uint8, string_to_array(s).dtype)
-        self.assertEqual(14, len(string_to_array(s)))
+        self.assertEqual(6, len(string_to_array(s)))
         self.assertEqual(
-            "ÿþh\x00i\x00 \x00Ç\x03 \x00.\x00",
+            # "ÿþh\x00i\x00 \x00Ç\x03 \x00.\x00",
+            "hi   .",
             "".join(map(chr, string_to_array(s))),
         )
         self.assertEqual(
-            [255, 254, 104, 0, 105, 0, 32, 0, 199, 3, 32, 0, 46, 0],
+            # [255, 254, 104, 0, 105, 0, 32, 0, 199, 3, 32, 0, 46, 0],
+            [104, 105, 32, 32, 32, 46],
             string_to_array(s).tolist(),
         )
