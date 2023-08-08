a23998b96d83ab9f47abab1419512633a464c3fb
John Hanley <jhanley708-git@s6.sector6.net>
2023-08-08 16:35:40 -0700

    Add caching add_doc_url()

--- gen/news_summary.py
+++ gen/news_summary.py
@@ -1,5 +1,37 @@
+from pathlib import Path
+
+from huggingface_hub import hf_hub_download
+import requests
+
+
 class Summarizer:
+    CACHE_DIR = Path("/tmp/cache")
 
+    @classmethod
+    def _get_cache_filespec(cls, url: str) -> Path:
+        cls.CACHE_DIR.mkdir(exist_ok=True)
 
-    def add_doc(self, param):
-        pass
+        basename = Path(url).name
+        if "." not in basename:
+            basename += ".html"
+        return cls.CACHE_DIR / basename
+
+    def add_doc_url(self, url: str) -> None:
+        fspec = self._get_cache_filespec(url)
+        if not fspec.exists():
+            ua = "Wget/1.21.4"
+            resp = requests.get(url, headers={"User-Agent": ua})
+            resp.raise_for_status()
+            ct = resp.headers["Content-Type"]
+            assert "text/html; charset=UTF-8" == ct, ct
+            assert "UTF-8" == resp.encoding, resp.encoding
+            # pp(dict(resp.headers))
+            fspec.write_bytes(resp.content)
+
+        self.add_doc(fspec.read_text(encoding="UTF-8"))
+
+    def add_doc_file(self, in_file: Path) -> None:
+        self.add_doc(in_file.read_text(encoding="UTF-8"))
+
+    def add_doc(self, text: str) -> None:
+        hf_hub_download(repo_id="google/pegasus-xsum", filename="config.json")
--- gen/news_summary_test.py
+++ gen/news_summary_test.py
@@ -1,12 +1,19 @@
+from pathlib import Path
 import unittest
 
 from gen.news_summary import Summarizer
 
 
 class SummarizerTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.s = Summarizer()
+
     def test_summarize(self):
-        s = Summarizer()
-        s.add_doc("hello world")
+        self.s.add_doc("hello world")
 
     def test_summarize_newsweek(self):
-        url ='https://www.newsweek.com/americas-most-responsible-companies-2022'
+        url = "https://www.newsweek.com/americas-most-responsible-companies-2022"
+        self.s.add_doc_url(url)
+
+        txt_file = Path("/tmp/cache/americas-most-responsible-companies-2022.txt")
+        self.s.add_doc_file(txt_file)
