1daf56cafa70bb5f286ae790ab6b5e30240cc8d5
John Hanley <jhanley708-git@s6.sector6.net>
2023-08-09 14:09:08 -0700

    Add get_t5_model helper

--- gen/news_summary.py
+++ gen/news_summary.py
@@ -3,18 +3,8 @@
 
 from pathlib import Path
 
-from datasets import load_dataset
-from huggingface_hub import hf_hub_download
-from transformers import (
-    AutoTokenizer,
-    DataCollatorForSeq2Seq,
-    T5ForConditionalGeneration,
-    T5Tokenizer,
-)
-import evaluate
-import numpy as np
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 import requests
-import rouge_score
 
 
 class Summarizer:
@@ -29,7 +19,7 @@ class Summarizer:
             basename += ".html"
         return cls.CACHE_DIR / basename
 
-    def add_doc_url(self, url: str) -> None:
+    def add_doc_url(self, url: str) -> str:
         fspec = self._get_cache_filespec(url)
         if not fspec.exists():
             ua = "Wget/1.21.4"
@@ -41,91 +31,30 @@ class Summarizer:
             # pp(dict(resp.headers))
             fspec.write_bytes(resp.content)
 
-        self.add_doc(fspec.read_text(encoding="UTF-8"))
+        return self.add_doc(fspec.read_text(encoding="UTF-8"))
 
-    def add_doc_file(self, in_file: Path) -> None:
-        self.add_doc(in_file.read_text(encoding="UTF-8"))
+    def add_doc_file(self, in_file: Path) -> str:
+        return self.add_doc(in_file.read_text(encoding="UTF-8"))
 
-    def add_doc(self, text: str) -> None:
-        hf_hub_download(repo_id="google/pegasus-xsum", filename="config.json")
-        billsum = load_dataset("billsum", split="ca_test")
-        billsum = billsum.train_test_split(test_size=0.2)
+    def add_doc(self, text: str, limit: int = 20) -> str:
+        text = "summarize: " + text[:1800]
 
-        from pprint import pp
+        tokenizer, model = get_t5_model()
+        input_ids = tokenizer(text, return_tensors="pt").input_ids
+        outputs = model.generate(input_ids, max_new_tokens=limit)
+        return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-        # pp(billsum["train"][0])
-        pp(list(billsum["test"][0].keys()))
-        x = self.preprocess(billsum["train"][0])
-        print(len(x), type(x))
-
-        tokenized_billsum = billsum.map(self.preprocess, batched=True)
-        print(tokenized_billsum, type(tokenized_billsum))
-        rouge = evaluate.load("rouge")
-        print(rouge, type(rouge))
-
-    def preprocess(self, examples):
-        checkpoint = "t5-small"
-        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-        prefix = "summarize: "
-
-        inputs = [prefix + doc for doc in examples["text"]]
-        model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
-
-        labels = tokenizer(
-            text_target=examples["summary"], max_length=128, truncation=True
-        )
-
-        model_inputs["labels"] = labels["input_ids"]
-
-        data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=checkpoint)
-        print(data_collator)
-
-        return model_inputs
-
-    # model = T5ForConditionalGeneration.from_pretrained("google/t5-small")
     # model = T5ForConditionalGeneration.from_pretrained("google/t5-v1_1-base")
 
-    def compute_metrics(self, eval_pred):
-        predictions, labels = eval_pred
-        checkpoint = "t5-small"
-        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-        result = rouge_score.compute(
-            predictions=decoded_preds, references=decoded_labels, use_stemmer=True
-        )
-
-        prediction_lens = [
-            np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions
-        ]
-        result["gen_len"] = np.mean(prediction_lens)
-
-        return {k: round(v, 4) for k, v in result.items()}
-
-
-def t5():
-    gen_model_name = "t5-small"
-    tokenizer = T5Tokenizer.from_pretrained(gen_model_name, legacy=False)
-    model = T5ForConditionalGeneration.from_pretrained(gen_model_name)
-
-    input_ids = tokenizer(
-        "The <extra_id_0> walks in <extra_id_1> park", return_tensors="pt"
-    ).input_ids
-    labels = tokenizer(
-        "<extra_id_0> cute dog <extra_id_1> the <extra_id_2>", return_tensors="pt"
-    ).input_ids
-
-    # the forward function automatically creates the correct decoder_input_ids
-    loss = model(input_ids=input_ids, labels=labels).loss
-    print(loss.item())
+def get_t5_model():
+    tokenizer = T5Tokenizer.from_pretrained("t5-small", legacy=False)
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
+    return tokenizer, model
 
 
 def translate():
-    tokenizer = T5Tokenizer.from_pretrained("t5-small", legacy=False)
-    model = T5ForConditionalGeneration.from_pretrained("t5-small")
-
+    tokenizer, model = get_t5_model()
     text = "translate English to German: The house is wonderful."
     input_ids = tokenizer(text, return_tensors="pt").input_ids
     outputs = model.generate(input_ids, max_new_tokens=20)
--- gen/news_summary_test.py
+++ gen/news_summary_test.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import unittest
 
+from datasets import Dataset, load_dataset
+from huggingface_hub import hf_hub_download
+import huggingface_hub
+
 from gen.news_summary import Summarizer
 
 
@@ -15,4 +19,30 @@ class SummarizerTest(unittest.TestCase):
         # self.s.add_doc_url(url)
 
         txt_file = Path("/tmp/cache/americas-most-responsible-companies-2022.txt")
-        self.s.add_doc_file(txt_file)
+        r = self.s.add_doc_file(txt_file)
+        self.assertEqual(
+            "a list of America's most Responsible Companies is being compiled by newsweek. the",
+            r,
+        )
+
+    def test_load_dataset(self):
+        billsum = load_dataset("billsum", split="ca_test")
+        self.assertIsInstance(billsum, Dataset)
+        self.assertEqual(
+            ["text", "summary", "title"],
+            list(billsum.features.keys()),
+        )
+        self.assertEqual(1237, billsum.num_rows)
+
+    def test_hf_datasets(self) -> None:
+        self.assertGreater(len(list(huggingface_hub.list_datasets())), 51_512)
+
+        # hf_hub_download(repo_id="lysandre/arxiv-nlp", filename="config.json")
+        fleurs = "fleurs.py"
+        fspec = Path(
+            hf_hub_download(
+                repo_id="google/fleurs", filename=fleurs, repo_type="dataset"
+            )
+        )
+        self.assertTrue(fspec.exists())
+        self.assertEqual(fleurs, fspec.name)
