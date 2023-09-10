228cd64b4c66754a64874c038d40402ccf89d778
John Hanley <jhanley708-git@s6.sector6.net>
2023-09-10 12:00:31 -0700

    Display a city block

--- geo/greenwave/demo.py
+++ geo/greenwave/demo.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
+from time import time
 
+from pygame import Rect, Surface, Vector2
 import pygame
+
+GRID_SIZE_PX: int = 3
 
 
 class City:
@@ -12,17 +16,34 @@ class City:
     which appear to be infinite sinks or sources.
     """
 
-    WIDTH: int = 4
-    HEIGHT: int = 3
-    BLOCK_SIZE: int = 20  # number of grids per (square) block
-    GRID_SIZE_PX: int = 3
+    BLOCK_SIZE: int = 50  # number of grids per (square) block
+
+    def __init__(self, width: int = 4, height: int = 3):
+        scale = self.BLOCK_SIZE * GRID_SIZE_PX
+        self.blocks = [
+            Block(150 + scale * i, 100 + scale * j)
+            for i in range(width)
+            for j in range(height)
+        ]
+
+
+class Block:
+    """A city block."""
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
 
 
 def main():
-    screen = pygame.display.set_mode((1280, 720))
+    city = City(1, 1)
     pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
-    running: bool = True
+    running = True
+    dt = 0
+
+    player_pos = Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
     while running:
         for event in pygame.event.get():
@@ -32,12 +53,22 @@ def main():
                         continue
                     running = False
 
-        screen.fill("purple")
+        screen.fill("grey")
+        draw(city, screen)
+        pygame.draw.circle(screen, "red", player_pos, 40)
 
-        pygame.display.flip()  # Refresh on-screen display
-        clock.tick(60)  # wait until next frame (at 60 FPS)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1e3  # FPS
+        if not (0.016 <= dt < 0.020):
+            print(dt, "\t", time())
 
     pygame.quit()
+
+
+def draw(city: City, screen: Surface):
+    size = city.BLOCK_SIZE * GRID_SIZE_PX - GRID_SIZE_PX
+    for block in city.blocks:
+        pygame.draw.rect(screen, "white", Rect((block.x, block.y), (size, size)))
 
 
 if __name__ == "__main__":
