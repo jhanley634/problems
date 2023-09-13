9a1b340f1333759564afbfb308bd187957c4974b
John Hanley <jhanley708-git@s6.sector6.net>
2023-09-12 17:27:45 -0700

    Add test_sorted_obstacles

--- geo/greenwave/tests/obstacle_test.py
+++ geo/greenwave/tests/obstacle_test.py
@@ -1,8 +1,24 @@
+from operator import attrgetter
 import unittest
 
-from geo.greenwave.demo import Car
+from sortedcontainers import SortedKeyList
+
+from geo.greenwave.demo import Car, City
 
 
 class ObstacleTest(unittest.TestCase):
     def test_sorted_obstacles(self) -> None:
-        car = Car(None, 1)
+        seg = City(2, 1).blocks[0].road_segments[0]
+
+        cars = SortedKeyList(key=attrgetter("position"))
+        for i in range(7):
+            cars.add(Car(seg, float(i)))
+            cars[-1].update(1.0)
+        self.assertEqual(7, len(cars))
+        self.assertEqual(
+            [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+            list(map(attrgetter("position"), cars)),
+        )
+
+        idx = cars.bisect_key_left(3.0)
+        self.assertEqual(7, idx)
