255c00ebd59f741e71a60b84f4682fc0bb410c46
John Hanley <jhanley708-git@s6.sector6.net>
2023-11-13 17:16:14 -0800

    Plot on a map

--- homl3/ch02/show_counties.py
+++ homl3/ch02/show_counties.py
@@ -9,13 +9,27 @@ import streamlit as st
 from homl3.ch02.find_nearest import join_on_location
 
 
-def show_counties():
-    housing = pd.DataFrame(join_on_location().drop(columns=["geometry"]))
-    mem_pandas = housing.memory_usage(deep=True).sum()
+def _get_housing():
+    housing_df = pd.DataFrame(join_on_location().drop(columns=["geometry"]))
+    assert max(housing_df["pop_density"]) == 50_983
+    mem_pandas = housing_df.memory_usage(deep=True).sum()
 
-    housing = pl.from_pandas(housing)
+    housing = pl.from_pandas(housing_df)
     mem_polars = housing.estimated_size()
-    print("memory ratio:", round(mem_pandas / mem_polars, 2))
+    print("memory ratio:", round(mem_pandas / mem_polars, 1))  # typ. 2.3
+    return housing
+
+
+def show_counties():
+    housing = _get_housing()
+    max_density = housing["pop_density"].max()
+    housing = housing.with_columns(size=500.0 * housing["pop_density"] / max_density)
+    drop_cols = ["median_house_value", "longitude", "latitude"]
+    print(housing.drop(columns=drop_cols).describe())
+    st.map(
+        housing,
+        size="size",
+    )
     st.write(housing)
 
 
