bc23e18cbe4cbc3acb8db8ce6d727eb95671872e
John Hanley <jhanley708-git@s6.sector6.net>
2023-11-16 23:12:14 -0800

    Add discard_interior_observations

--- homl3/ch02/county_voronoi.py
+++ homl3/ch02/county_voronoi.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python
-
 # Copyright 2023 John Hanley. MIT licensed.
 
-
-from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial import ConvexHull, Voronoi, voronoi_plot_2d
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from homl3.ch02.show_counties import color_of, get_housing
 
@@ -13,6 +12,10 @@ def show_voronoi(want_show=False):
     housing = get_housing().to_pandas()  # We need pandas, for color support.
     housing["color"] = housing["county"].apply(color_of)
     assert len(set(housing.county)) == 58
+
+    housing["interior"] = False
+    housing = discard_interior_observations(housing)
+    print(len(housing))
 
     points = housing[["longitude", "latitude"]].to_numpy()
     assert points.shape == (20640, 2)  # dtype is float64
@@ -23,6 +26,25 @@ def show_voronoi(want_show=False):
     fig.savefig("/tmp/voronoi.png")
     if want_show:
         plt.show()
+        print(housing[["county", "population"]].groupby("county").count())
+        # San_Joaquin has 422 census tract observations; L.A. has 5861
+
+
+def discard_interior_observations(housing: pd.DataFrame) -> pd.DataFrame:
+    county = "Los Angeles"
+    print(len(housing[housing.county == county]))
+
+    print(sum(housing[housing.county == county].interior))
+    # housing.loc[housing.county == county].interior = True
+    print(sum(housing[housing.county == county].interior))
+
+    points = housing[["longitude", "latitude"]].to_numpy()
+    hull = ConvexHull(points)
+    print(hull.volume)
+    print(hull)
+    # breakpoint()
+
+    return housing
 
 
 if __name__ == "__main__":
