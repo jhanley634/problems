f6a41d92114c04642002caa2911fa2a1e63c0db5
John Hanley <jhanley708-git@s6.sector6.net>
2023-11-26 12:56:36 -0800

    Add retiring.py

---
+++ geo/zone/retiring.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# Copyright 2023 John Hanley. MIT licensed.
+
+from typing import Generator
+import re
+
+import pandas as pd
+
+# from lynx -dump of:
+# https://www.nytimes.com/2023/11/26/us/politics/congress-retirements-list.html
+
+
+def get_retiring(
+    in_file="/tmp/congress-retirements-list.txt", verbose=False
+) -> Generator[dict[str, str], None, None]:
+    """Reports on retiring congress folks."""
+    rep_re = re.compile(r"(Representative|Senator) (.+), (Republican|Democrat) of (.+)")
+    with open(in_file, encoding="ISO-8859-1") as fin:
+        for line in fin:
+            m = rep_re.search(line)
+            if m:
+                type_, name, party, state = m.groups()
+                yield dict(type=type_, party=party, state=state, name=name)
+                if verbose:
+                    print(f"{party:11} {type_:15} {state:15} {name}")
+
+
+if __name__ == "__main__":
+    df = pd.DataFrame(get_retiring())
+    cols = list(map(str, df.columns))
+    print(df.sort_values(cols).to_markdown(index=False))
--- homl3/ch02/county_voronoi_test.py
+++ homl3/ch02/county_voronoi_test.py
@@ -1,3 +1,5 @@
+# Copyright 2023 John Hanley. MIT licensed.
+
 import unittest
 
 import pandas as pd
