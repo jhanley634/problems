a66c86836730f9c3d90c1a2a4af4cf36729afca5
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-16 14:32:13 -0800

    Add insert_world_facts

--- geo/zone/so/article_db.py
+++ geo/zone/so/article_db.py
@@ -3,13 +3,17 @@
 
 # from https://softwareengineering.stackexchange.com/questions/450146/designing-a-graph-database-structure
 
+from array import array
 from hashlib import sha3_224
 from pathlib import Path
+from random import shuffle
 from time import time
 from typing import Any, Callable
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy.schema import PrimaryKeyConstraint
+from tqdm import tqdm
 import pandas as pd
 import sqlalchemy as sa
 
@@ -46,6 +50,13 @@ class WorldFact(Base):
     details = Column(String)
 
 
+class Fact(Base):
+    __tablename__ = "fact"
+    user_id = Column(Integer)
+    fact_id = Column(Integer, ForeignKey("world_fact.id"))
+    PrimaryKeyConstraint(user_id, fact_id)
+
+
 def create_engine():
     DB_FILE = Path("/tmp/article.db")
     DB_URL = f"sqlite:///{DB_FILE}"
@@ -55,25 +66,45 @@ def create_engine():
 
 engine = create_engine()
 
-NUM_FACTS = 188_000  # It takes 1 second to INSERT this many rows.
+# It takes 1 second to INSERT 188_000 rows into world_facts.
+NUM_FACTS = 40_000
+NUM_USERS = 10_000
 
 
-def get_df():
+def get_fact_df():
     return pd.DataFrame(
         {
             "name": [
                 sha3_224(f"Earth {i}".encode()).hexdigest() for i in range(NUM_FACTS)
             ],
-            "details": [f"lorem ipsum {i}" for i in range(NUM_FACTS)],
+            "details": [f"Loxodonta africana {i}" for i in range(NUM_FACTS)],
         }
     )
 
 
 @timed
-def main(df: pd.DataFrame) -> None:
-    df.to_sql("world_fact", engine, index=False, if_exists="append")
+def insert_world_facts(fact_df: pd.DataFrame) -> None:
+    fact_df.to_sql("world_fact", engine, index=False, if_exists="append")
+
+
+def insert_user_facts(user_df: pd.DataFrame) -> None:
+    user_df.to_sql("fact", engine, index=False, if_exists="append")
+
+
+def main():
+    fact_df = get_fact_df()
+    insert_world_facts(fact_df)
+
+    with Session(engine) as session:
+        fact_ids = array("I", [row.id for row in session.query(WorldFact.id)])
+
+    for userid in tqdm(range(NUM_USERS), smoothing=1e-4):
+        shuffle(fact_ids)
+        user_df = pd.DataFrame({"fact_id": fact_ids[: NUM_FACTS // 2]})
+        user_df["user_id"] = userid
+        insert_user_facts(user_df)
 
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
-    main(get_df())
+    main()
