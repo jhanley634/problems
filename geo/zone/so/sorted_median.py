0e433694e4874fd3d8b0377ebd561278e9a0285f
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-22 13:19:03 -0800

    Use numpy flip()

--- geo/zone/so/sorted_median.py
+++ geo/zone/so/sorted_median.py
@@ -5,23 +5,31 @@ from random import randrange
 from time import time
 import unittest
 
+from tqdm import tqdm
 import numpy as np
 
 
 class SortedMedianTest(unittest.TestCase):
     def setUp(self, n: int = 10_000) -> None:
-        self.xs = [randrange(n) for _ in range(n)]
+        self.rand = [randrange(n) for _ in range(10 * n)]
 
-    # typical array speedup is 3x: 12.906 s / 4.376 s == 2.95
+    # typical array speedup is 3x: 21.764 s / 6.993 s
     def test_sort_speed_list(self) -> None:
         t0 = time()
-        for _ in range(len(self.xs)):
-            self.xs.copy().sort()
+        for _ in tqdm(range(len(self.rand))):
+            a = self.rand.copy()
+            a.sort()
+            a.sort(reverse=True)
+            a.sort()
         print(f"\nlist sort: {time() - t0:.3f} sec")
 
     def test_sort_speed_array(self) -> None:
-        xs = np.array(self.xs)
+        xs = np.array(self.rand)
         t0 = time()
-        for _ in range(len(self.xs)):
-            np.array(xs).sort()
+        for _ in tqdm(range(len(self.rand))):
+            a = np.array(xs)
+            a.sort()
+            a = np.flip(a)
+            a.sort()
+            a.sort()
         print(f"\narray sort: {time() - t0:.3f} sec")
