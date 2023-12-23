f5bcb46e9b2abf4f56ecdacea1f68483a3c153b5
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-23 09:36:38 -0800

    Add ListName enum

--- geo/zone/so/sorted_median.py
+++ geo/zone/so/sorted_median.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # Copyright 2023 John Hanley. MIT licensed.
 
+from enum import Enum, auto
 from random import randrange
 from time import time
 import unittest
@@ -8,11 +9,16 @@ import unittest
 import numpy as np
 
 
+class ListName(Enum):
+    X = auto()
+    Y = auto()
+
+
 def _monotonic(a: np.ndarray[int, np.dtype[np.int_]]) -> bool:
     return bool(np.all(np.diff(a) >= 0))
 
 
-def median_idx_of_one_list(xs: np.ndarray[int, np.dtype[np.int_]]) -> int:
+def median_idx_of_single_list(xs: np.ndarray[int, np.dtype[np.int_]]) -> int:
     assert len(xs) > 0
     assert len(xs) % 2 == 1
     assert _monotonic(xs)
@@ -22,31 +28,42 @@ def median_idx_of_one_list(xs: np.ndarray[int, np.dtype[np.int_]]) -> int:
     return mid
 
 
-def median_of_two_lists(
+def median_of_list_pair(
     xs: np.ndarray[int, np.dtype[np.int_]],
     ys: np.ndarray[int, np.dtype[np.int_]],
 ) -> int:
     assert len(xs) > 0
-    assert len(xs) % 2 == 1
     assert _monotonic(xs)
 
     assert len(ys) > 0
-    assert len(ys) % 2 == 1
     assert _monotonic(ys)
 
-    mid = len(xs) // 2
-    return mid
+    assert (len(xs) + len(ys)) % 2 == 1  # The answer is definitely one of the elements.
+
+    x_mid = len(xs) // 2
+    y_mid = len(xs) // 2
+
+    return (x_mid + y_mid) // 2
 
 
 class SortedMedianTest(unittest.TestCase):
     def setUp(self, n: int = 1_001) -> None:
         self.rand = [randrange(int(1.5 * n)) for _ in range(n)]
 
-    def test_median_of_one_list(self) -> None:
+    def test_median_of_single_list(self) -> None:
         xs = np.array(sorted(self.rand))
         med_val = np.quantile(xs, 0.5)
-        i = median_idx_of_one_list(xs)
+        i = median_idx_of_single_list(xs)
         self.assertEqual(med_val, xs[i])
+
+    def test_median_of_list_pair(self) -> None:
+        n = len(self.rand)
+        xs = np.array(sorted(randrange(int(1.5 * n)) for _ in range(n)))
+        ys = np.array(sorted(randrange(int(1.5 * n)) for _ in range(n - 1)))
+        both = np.array(sorted(np.concatenate((xs, ys))))
+        med_val = np.quantile(both, 0.5)
+        i = len(both) // 2
+        self.assertEqual(med_val, both[i])
 
     # typical array speedup is 3x:idx_ 21.764 s / 6.993 s
     def test_sort_speed_list(self) -> None:
