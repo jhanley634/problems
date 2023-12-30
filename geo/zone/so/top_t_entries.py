229ef5bf8f97bd3892b3ec60e252dc20405f6748
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-29 18:05:55 -0800

    Add test_with_hypothesis

--- geo/zone/so/top_t_entries.py
+++ geo/zone/so/top_t_entries.py
@@ -5,12 +5,21 @@
 # cf https://stackoverflow.com/questions/52713266/most-efficient-way-to-get-the-largest-3-elements-of-an-array-using-no-comparison
 
 from collections import Counter
+from contextlib import contextmanager
 import unittest
 
 from beartype import beartype
+from hypothesis import given
+from hypothesis import strategies as st
 import numpy as np
 
-cnt = Counter()  # an event counter
+cnt: Counter = Counter()  # an event counter
+
+
+@contextmanager
+def count_sort_calls():
+    cnt["sort"] = 0
+    yield cnt
 
 
 @beartype
@@ -29,20 +38,34 @@ K = 5
 
 
 @beartype
-def sort_k(a: np.ndarray, start: int, k: int = K) -> None:
+def sort_k(a: np.ndarray, start: int, k: int) -> None:
     """Puts K elements into descending order."""
     # The "top" elements will appear at the front.
     # This is convenient for truncated displays of large arrays.
     assert 0 <= start < len(a)
-    assert 0 < k <= len(a)
     cnt["sort"] += 1
 
     a[start : start + k] = sorted(a[start : start + k], reverse=True)
 
 
 @beartype
-def find_top_t(t: int, a: np.ndarray) -> np.ndarray:
-    0
+def find_top_t(t: int, a: np.ndarray, k: int = K) -> np.ndarray:
+    assert t < K
+    assert t < len(a)
+    tombstone = a.min() - 1  # sentinel value
+    assert tombstone not in a  # true by construction
+
+    while len(a) > t:
+        for i in range(0, len(a), k):
+            sort_k(a, i, k)
+            a[i + t : i + k] = tombstone
+        # Now coalesce the survivors, shrinking the array of candidate answer values.
+        a = np.array(list(filter(lambda x: x != tombstone, a)))
+
+    return a[:t]
+
+
+T = 3
 
 
 class TestTopT(unittest.TestCase):
@@ -52,7 +75,28 @@ class TestTopT(unittest.TestCase):
 
     def test_sorted_slice(self) -> None:
         a = np.array(range(8))
+
         sort_k(a, 2, k=3)
         self.assertEqual([0, 1, 4, 3, 2, 5, 6, 7], a.tolist())
-        sort_k(a, 2)
+
+        sort_k(a, 2, K)
         self.assertEqual([0, 1, 6, 5, 4, 3, 2, 7], a.tolist())
+
+        a.sort()
+        sort_k(a, 6, K)  # Partial sort, at end of array, works as expected.
+        self.assertEqual([0, 1, 2, 3, 4, 5, 7, 6], a.tolist())
+
+    def test_top_t(self, t=T) -> None:
+        a = roll_some_numbers(10)
+        with count_sort_calls() as cnt:
+            xs = find_top_t(t, a.copy()).tolist()
+        self.assertEqual(5, cnt["sort"])
+        self.assertEqual(sorted(xs, reverse=True), xs)
+        self.assertEqual(sorted(a, reverse=True)[:t], xs)
+
+    @given(st.lists(st.integers(), min_size=T, max_size=100))
+    def test_with_hypothesis(self, lst: list[int]) -> None:
+        a = np.array(lst)
+        xs = find_top_t(T, a.copy()).tolist()
+        self.assertEqual(sorted(xs, reverse=True), xs)
+        self.assertEqual(sorted(a, reverse=True)[:T], xs)
