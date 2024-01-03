4b73433c80f561195ba364f55ee5400fbda17161
John Hanley <jhanley708-git@s6.sector6.net>
2024-01-02 18:26:53 -0800

    Add _get_gpx_points

--- geo/gpx/route_movie.py
+++ geo/gpx/route_movie.py
@@ -1,13 +1,43 @@
+#! /usr/bin/env python
 #! /usr/bin/env streamlit run --server.runOnSave true
 # Copyright 2024 John Hanley. MIT licensed.
 
+from pathlib import Path
+from typing import Generator
+import datetime as dt
+
+from geopy import Point
+from gpxpy.gpx import GPX
+import gpxpy
 
 from geo.gpx.route_viz import _display, _get_chosen_gpx_path, _get_df
 from geo.ski.dwell import get_breadcrumbs
 
 
 def main() -> None:
-    _display(_get_df(_get_chosen_gpx_path()))
+    in_file = Path("~/Desktop/gpx.d/2023-12-07T22:20:02Z-willows-walk.gpx").expanduser()
+
+    for point in _get_gpx_points(in_file):
+        print(point)
+
+    # _display(_get_df(_get_chosen_gpx_path()))
+
+
+def _get_gpx_points(in_file: Path) -> Generator[Point, None, None]:
+    # Similar to _get_breadcrumbs()
+    with open(in_file) as fin:
+        gpx: GPX = gpxpy.parse(fin)
+        for row in gpx.tracks:
+            for segment in row.segments:
+                for pt in segment.points:
+                    stamp: dt.datetime = pt.time
+                    stamp = stamp.replace(tzinfo=None).astimezone(dt.timezone.utc)
+                    yield dict(
+                        stamp=stamp,
+                        lat=round(pt.latitude, 6),
+                        lng=round(pt.longitude, 6),
+                        elevation=round(pt.elevation, 2),
+                    )
 
 
 if __name__ == "__main__":
