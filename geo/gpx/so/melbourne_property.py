fc9b7213b0c6f775dd14b14e99bad1de53348d3e
John Hanley <jhanley708-git@s6.sector6.net>
2024-01-05 19:59:02 -0800

    Add melbourne_property.py BS4 parsing example

---
+++ geo/gpx/so/melbourne_property.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright 2024 John Hanley. MIT licensed.
+
+# from https://stackoverflow.com/questions/77768127/accessing-nested-element-using-beautifulsoup
+
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+import typer
+
+url = "https://gist.github.com/sachinruk/9b0aaf5c134aa398f7f201c2b298210a"
+raw = (
+    "https://gist.githubusercontent.com/sachinruk/9b0aaf5c134aa398f7f201c2b298210a"
+    "/raw/2b255fe028f723ed2afbfa7e2679cd0de56b4685/property.html"
+)
+
+
+def parse(in_file: Path) -> None:
+    hrule = "=" * 60
+    soup = BeautifulSoup(in_file.read_text(), "html.parser")
+    ol = soup.find("ol", {"class": "messageList", "id": "messageList"})
+    for li in ol.find_all("li"):
+        print(f"\n\n{hrule}\n\n{li}")
+
+
+if __name__ == "__main__":
+    typer.run(parse)
