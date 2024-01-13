9dd27bf5b0c299b35045f0b305dc4debda933025
John Hanley <jhanley708-git@s6.sector6.net>
2024-01-12 19:01:26 -0800

    Pipe to "column -t" for auto column widths

--- infra/blood_pressure.py
+++ infra/blood_pressure.py
@@ -1,14 +1,18 @@
 #! /usr/bin/env python
 # Copyright 2023 John Hanley. MIT licensed.
+
 from pathlib import Path
+from subprocess import PIPE, Popen
 from typing import Generator
 import csv
+import io
 
-import pandas as pd
 import polars as pl
 
 FOLDER = Path("~/Desktop").expanduser()
-IN_FILE = list(FOLDER.glob("Your Requested OMRON Report from *.csv"))[0]
+# Grab the most recent observations.
+IN_FILE = list(FOLDER.glob("Your Requested OMRON Report from *.csv"))[-1]
+OUT_FILE = FOLDER / "blood_pressure.csv"
 
 
 def _get_rows(in_file: Path) -> Generator[dict[str, str | int], None, None]:
@@ -27,12 +31,18 @@ def _get_rows(in_file: Path) -> Generator[dict[str, str | int], None, None]:
             yield dict(time=stamp, systolic=s, diastolic=d, pulse=p)
 
 
-def main(in_file: Path = IN_FILE) -> None:
-    df = pd.DataFrame(list(_get_rows(in_file)))
-    df["time"] = pd.to_datetime(df.time)
-    print(df)
-    print(df.dtypes)
-    print(df.describe())
+def main(in_file: Path = IN_FILE, out_file: Path = OUT_FILE) -> None:
+    df = pl.DataFrame(list(_get_rows(in_file)))
+    df = df.with_columns(pl.col("time").str.to_datetime("%b %d %Y %I:%M %p"))
+    df = df.with_columns(df["time"].dt.strftime("%Y-%m-%dT%H:%M"))
+
+    buf = io.BytesIO()
+    df.write_csv(buf)
+    txt = buf.getvalue().decode().replace(",", ", ")  # now we have words
+
+    p = Popen(["column", "-t"], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    result = p.communicate(input=txt.encode())[0].decode()
+    print(result.replace("T", " "))  # turns timestamp into two words
 
 
 if __name__ == "__main__":
