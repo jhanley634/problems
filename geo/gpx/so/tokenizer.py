e8be6923c79aecf5e88b5bb4f23cb5c4ceee2fa6
John Hanley <jhanley708-git@s6.sector6.net>
2024-01-25 07:05:59 -0800

    Add spacy_wordlist

--- geo/gpx/so/tokenizer.py
+++ geo/gpx/so/tokenizer.py
@@ -4,19 +4,22 @@
 # from https://codereview.stackexchange.com/questions/289102/tokenize-a-file
 
 from collections import Counter
-import pprint
-import sys
+from pathlib import Path
+from pprint import pp
+from typing import TextIO
 
 import spacy
+import spacy.tokens
+import typer
 
 
-def spacy_wordlist() -> None:
+def spacy_wordlist(fin: TextIO) -> None:
     nlp = spacy.load("en_core_web_sm")
     seen = set()
     cnt = Counter()
 
-    for line in sys.stdin:
-        doc = (word for word in nlp(line.strip()) if word.is_alpha)
+    for line in fin:
+        doc = (word for word in nlp(line.lower().strip()) if word.is_alpha)
 
         for token in doc:
             cnt[token.text] += 1
@@ -24,8 +27,14 @@ def spacy_wordlist() -> None:
                 seen.add(token.text)
                 print(token.text)
 
-    pprint.pp(cnt)
+    print("\n".join(sorted(seen)))
+    pp(cnt)
+
+
+def main(in_file: Path) -> None:
+    with open(in_file) as fin:
+        spacy_wordlist(fin)
 
 
 if __name__ == "__main__":
-    spacy_wordlist()
+    typer.run(main)
