dd070a06747ccdb88d7920f9a79dcf0d63c42553
John Hanley <jhanley708-git@s6.sector6.net>
2024-01-27 07:27:18 -0800

    Add get_input

--- geo/gpx/so/tokenizer/src/main.rs
+++ geo/gpx/so/tokenizer/src/main.rs
@@ -1,5 +1,20 @@
+
+use std::io::{self, Write};
+
+fn get_input() -> String {
+    print!("Plain text:  ");
+    io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf
+}
+
+
 fn main() {
     let i = 42;
     println!("Hello, world!");
+    let line = get_input();
+    println!(">{}<", line.trim_end());
     println!("{}", i);
 }
