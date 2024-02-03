fbc8bfcde7b2a4b2b0880ea61c641801fe28f8b6
John Hanley <jhanley708-git@s6.sector6.net>
2024-02-03 08:45:15 -0800

    Add call to findCountours()

--- geo/gpx/so/square_finder.py
+++ geo/gpx/so/square_finder.py
@@ -29,12 +29,23 @@ def main() -> None:
     assert (1671, 928) == img.size
     assert "L" == img.mode
 
-    a = np.array(img.getdata())
+    a = np.array(img.getdata(), dtype=np.uint8).reshape(tuple(reversed(img.size)))
     print(a.shape, a.dtype, a.min(), a.max())
 
-    CreateImageHeader(img.size, IPL_DEPTH_8U, 3)
+    thresh = cv2.threshold(a, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-    # img.show()
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.015 * peri, True)
+        if len(approx) == 4:
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(a, (x, y), (x + w, y + h), (36, 255, 12), 2)
+
+    img = Image.fromarray(thresh)
+    print(img)
+    img.show()
 
 
 if __name__ == "__main__":
