5f9cbdeaf22ebf7606d78987f94408ee8398fa9d
John Hanley <jhanley708-git@s6.sector6.net>
2024-02-04 06:53:27 -0800

    Add (faster) crc32_stm_bis

--- geo/gpx/so/stm_crc32.py
+++ geo/gpx/so/stm_crc32.py
@@ -4,13 +4,14 @@
 
 # from https://codereview.stackexchange.com/questions/176032/python-code-to-calculate-stm-crc32
 
+from array import array
 from hashlib import sha3_512
 from time import time
 import unittest
 
 
-def generate_crc32_table(poly):
-    custom_crc_table = {}
+def generate_crc32_table(poly: int) -> dict[int, int]:
+    custom_crc_table: dict[int, int] = {}
     for i in range(256):
         c = i << 24
 
@@ -26,7 +27,7 @@ poly = 0x04C1_1DB7
 custom_crc_table = generate_crc32_table(poly)
 
 
-def crc32_stm(bytes_arr):
+def crc32_stm(bytes_arr: bytes) -> int:
     length = len(bytes_arr)
     crc = 0xFFFF_FFFF
 
@@ -38,7 +39,6 @@ def crc32_stm(bytes_arr):
             | ((bytes_arr[k + 2] << 8) & 0xFF00)
             | (bytes_arr[k + 3] & 0xFF)
         )
-
         crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[0xFF & ((crc >> 24) ^ v)]
         crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[
             0xFF & ((crc >> 24) ^ (v >> 8))
@@ -82,18 +82,76 @@ def crc32_stm(bytes_arr):
     return crc
 
 
+def crc32_stm_bis(bytes_arr: bytes) -> int:
+    length = len(bytes_arr)
+    crc = 0xFFFF_FFFF
+
+    a = array("I", bytes_arr[: length - length % 4])
+    a.byteswap()
+    for v in a:
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[0xFF & ((crc >> 24) ^ v)]
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[
+            0xFF & ((crc >> 24) ^ (v >> 8))
+        ]
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[
+            0xFF & ((crc >> 24) ^ (v >> 16))
+        ]
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[
+            0xFF & ((crc >> 24) ^ (v >> 24))
+        ]
+
+    k = 4 * len(a)
+    length -= k
+
+    if length > 0:
+        v = 0
+
+        for i in range(length):
+            v |= bytes_arr[k + i] << 24 - i * 8
+
+        if length == 1:
+            v &= 0xFF00_0000
+
+        elif length == 2:
+            v &= 0xFFFF_0000
+
+        elif length == 3:
+            v &= 0xFFFF_FF00
+
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[0xFF & ((crc >> 24) ^ (v))]
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[
+            0xFF & ((crc >> 24) ^ (v >> 8))
+        ]
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[
+            0xFF & ((crc >> 24) ^ (v >> 16))
+        ]
+        crc = ((crc << 8) & 0xFFFF_FFFF) ^ custom_crc_table[
+            0xFF & ((crc >> 24) ^ (v >> 24))
+        ]
+
+    return crc
+
+
 class StmCrc32Test(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         h = sha3_512(b"hello")
         assert h.hexdigest().startswith("75d527c3")
         assert 64 == len(h.digest())
         self.buf = b"".join([h.digest()] * 80)
         assert 5120 == len(self.buf)
 
-    def test_original(self):
+    def test_original(self) -> None:
         t0 = time()
         self.assertEqual(0xAB30_8DE0, crc32_stm(b"123456788"))
         self.assertEqual(0xAFF1_9057, crc32_stm(b"123456789"))
         for _ in range(10):
             self.assertEqual(0x957C_B03E, crc32_stm(self.buf))
+        print(f"\nelapsed: {time() - t0:.6f}")
+
+    def test_bis(self) -> None:
+        t0 = time()
+        self.assertEqual(0xAB30_8DE0, crc32_stm_bis(b"123456788"))
+        self.assertEqual(0xAFF1_9057, crc32_stm_bis(b"123456789"))
+        for _ in range(10):
+            self.assertEqual(0x957C_B03E, crc32_stm_bis(self.buf))
         print(f"\nelapsed: {time() - t0:.6f}")
