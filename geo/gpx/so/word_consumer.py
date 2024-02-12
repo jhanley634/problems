f83103db4059a7224cef6b0abd4b5c6bb2ede55e
John Hanley <jhanley708-git@s6.sector6.net>
2024-02-12 10:14:27 -0800

    Turn word_consumer into a generator

--- geo/gpx/so/word_consumer.py
+++ geo/gpx/so/word_consumer.py
@@ -2,17 +2,23 @@
 # Copyright 2024 John Hanley. MIT licensed.
 from collections import Counter
 from time import sleep
+from typing import Generator
 
 import redis
 
 
-def word_consumer(topic: str = "word-event") -> Counter[str]:
+def read_words(topic: str = "word-event") -> Counter[str]:
+    for _ in word_consumer(topic, category := Counter()):
+        pass
+    return category
+
+
+def word_consumer(topic: str, category: Counter[str]) -> Generator[str, None, None]:
     ps = redis.Redis().pubsub()
     ps.subscribe(topic)
     sleep(0.010)
     assert ps.get_message()["type"] == "subscribe"
 
-    category: Counter[str] = Counter()
     while True:
         msg = ps.get_message(timeout=0.1)
         if msg and msg["type"] == "message":
@@ -23,12 +29,13 @@ def word_consumer(topic: str = "word-event") -> Counter[str]:
             print(data.ljust(23), end="\t")
             if data == "request:  EOF":
                 print("\nBye!")
-                return category
+                return
             cat, _ = data.split(":")  # e.g. "item:     the"
             category[cat] += 1
+            yield data
 
 
 if __name__ == "__main__":
-    category = word_consumer()
+    category = read_words()
     assert 2 == len(category)
     assert 58_470 == category["item"] == category["request"]
