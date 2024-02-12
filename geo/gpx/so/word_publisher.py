e9f4bee1528a36b51fe0917f59ad690fcfa36565
John Hanley <jhanley708-git@s6.sector6.net>
2024-02-12 08:57:51 -0800

    Add words_in_common

--- geo/gpx/so/word_publisher.py
+++ geo/gpx/so/word_publisher.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 # Copyright 2024 John Hanley. MIT licensed.
 from collections import Counter
+from operator import itemgetter
 from pathlib import Path
 from typing import Generator
 import io
+import json
+import re
 
 from requests_cache import CachedSession
 import redis
@@ -19,7 +22,7 @@ def get_words(url: str) -> Generator[str, None, None]:
 
 def get_document(url: str, expire: int = 86400) -> str:
     session = CachedSession(cache_name="/tmp/requests_cache", expire_after=expire)
-    return no_bom(session.get(url).text)
+    return no_bom(dumb_quotes(session.get(url).text))
 
 
 def no_bom(s: str) -> str:
@@ -29,21 +32,58 @@ def no_bom(s: str) -> str:
     # return s.lstrip("\ufeff")
 
 
-def main(verbose: bool = False) -> None:
+def dumb_quotes(s: str) -> str:
+    clean = str.maketrans(
+        "™_—éê•",
+        "  -ee*",
+    )
+    single = re.compile("[‘’]")
+    double = re.compile("[“”]")
+    return double.sub('"', single.sub("'", s)).translate(clean)
+
+
+def main() -> None:
     tom = "https://www.gutenberg.org/ebooks/74.txt.utf-8"
     huck = "https://www.gutenberg.org/ebooks/76.txt.utf-8"
 
     assert 421_351 == len(get_document(tom))
     assert 602_756 == len(get_document(huck))
+    assert 8_002 == get_document(tom).index('"Tom!"')
 
-    tom_cnt = Counter(get_words(tom))
-    assert 3_923 == tom_cnt["the"]
+    both = words_in_common(
+        Counter(get_words(tom)),
+        Counter(get_words(huck)),
+    )
 
-    huck_cnt = Counter(get_words(huck))
-    assert 5_026 == huck_cnt["the"]
 
-    if verbose:
-        print(huck_cnt.most_common(8))
+def words_in_common(tom_cnt: Counter[str], huck_cnt: Counter[str]) -> Counter[str]:
+    assert 3_985 == tom_cnt["the"]
+    assert 3_190 == tom_cnt["and"]
+    assert 7_528 == len(tom_cnt)
+
+    assert 5_101 == huck_cnt["the"]
+    assert 6_440 == huck_cnt["and"]
+    assert 6_363 == len(huck_cnt)
+
+    both = Counter(
+        {
+            word: min(tom_cnt[word], huck_cnt[word])
+            for word in (tom_cnt.keys() & huck_cnt.keys())
+        }
+    )
+    # Now finesse the sort order, popular first then finally
+    # alphabetic hapax legomena, exploiting stable sort.
+    both = Counter({k: v for k, v in sorted(both.items(), key=itemgetter(1, 0))})
+    both = Counter(
+        {k: v for k, v in sorted(both.items(), key=itemgetter(1), reverse=True)}
+    )
+    assert 3_594 == len(both)
+    assert tom_cnt["the"] == both["the"]
+    assert tom_cnt["and"] == both["and"]
+
+    with open(Path("/tmp/both.json"), "w") as fout:
+        json.dump(both, fout, indent=2)
+    return both
 
 
 if __name__ == "__main__":
