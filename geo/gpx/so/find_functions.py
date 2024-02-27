1cf7cfb386fc847b80a62ae282fc7abf36ae103a
John Hanley <jhanley708-git@s6.sector6.net>
2024-02-27 10:17:48 -0800

    Add test_find_functions_under

--- geo/gpx/so/find_functions.py
+++ geo/gpx/so/find_functions.py
@@ -3,8 +3,8 @@
 # from https://stackoverflow.com/questions/78067735/statically-inspect-a-python-test-suite
 
 from pathlib import Path
-from types import FunctionType, ModuleType
-from typing import Callable, Generator, NamedTuple
+from types import FunctionType, MethodType, ModuleType
+from typing import Callable, Generator, Iterable, NamedTuple
 from unittest import TestCase
 from unittest.main import TestProgram
 import re
@@ -12,12 +12,15 @@ import sys
 
 
 def find_callable_functions(module: ModuleType | type) -> list[Callable]:
-    """Finds all callable functions in a module."""
+    """Finds callables within a module, including functions and classes."""
     return [
         obj
         for obj in module.__dict__.values()
-        if callable(obj) and isinstance(obj, (FunctionType, type))
+        if callable(obj) and isinstance(obj, (FunctionType, MethodType, type))
     ]
+
+
+# cf inspect.{isfunction, ismethod, isclass}
 
 
 class Source(NamedTuple):
@@ -37,11 +40,21 @@ def find_functions_in(source_file: Path) -> Generator[Source, None, None]:
             if record_delimiter.match(line):
                 if record.line > 0:
                     yield record
-                record = Source(file=source_file, line=i + 1, src=[])
+                record = Source(file=source_file.resolve(), line=i + 1, src=[])
             if not decorator.match(line):
                 record.src.append(line)
         if record.line > 0:
             yield record
+
+
+def find_functions_under(
+    paths: Iterable[Path], needle
+) -> Generator[Source, None, None]:
+    for path in paths:
+        if path.is_file() and path.suffix == ".py":
+            for record in find_functions_in(path):
+                if needle in "".join(record.src):
+                    yield record
 
 
 class FirstClass:
@@ -90,4 +103,16 @@ class TestFindFunctions(TestCase):
 
     def test_find_functions(self) -> None:
         source_records = list(find_functions_in(Path(__file__)))
-        self.assertEqual(12, len(source_records))
+        self.assertEqual(14, len(source_records))
+
+    def test_find_functions_under(self, verbose: bool = False) -> None:
+        source_folder = Path(__file__).parent
+        glob = source_folder.glob("**/*.py")
+
+        records = list(find_functions_under(glob, "generate_scenario"))
+        self.assertEqual(4, len(records))
+
+        if verbose:
+            for record in records:
+                print(record[:2])
+                print("".join(record.src))
