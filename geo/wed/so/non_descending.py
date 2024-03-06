161f2d48d86ac7e6384dec18e0d046a19bd8bbc2
John Hanley <jhanley708-git@s6.sector6.net>
2024-03-06 11:56:44 -0800

    Simplify -- there's no need to maintain the index i

--- geo/wed/so/non_descending.py
+++ geo/wed/so/non_descending.py
@@ -3,36 +3,42 @@
 
 # from https://codereview.stackexchange.com/questions/290899/leetcode-steps-to-make-array-non-decreasing
 
+from collections.abc import Iterable
 import unittest
 
+from beartype import beartype
 
-def total_steps(a: list[int]) -> int:
+
+@beartype
+def total_steps(a: Iterable[int]) -> int:
     total_count = count = 0
     if not a:  # not strictly needed, since input is specified to be nonempty
         return total_count
 
     # lowest acceptable number for remaining entries to the right
-    lowest = a[0]
+    nums = iter(a)
+    lowest = next(nums)
 
-    i = 1
-    while i < len(a):
-        if a[i] < lowest:
+    for val in nums:
+        if val < lowest:
             count += 1  # extend a "discard" span of entries
         else:
             total_count = max(total_count, count)
             count = 0  # we're back to accepting monotonic entries
 
-        lowest = max(lowest, a[i])
-        i += 1
+        lowest = max(lowest, val)
 
     return max(total_count, count)
 
 
 class NonDescendingTest(unittest.TestCase):
     def test_non_descending(self) -> None:
+        self.assertEqual(0, total_steps(range(0)))
+        self.assertEqual(0, total_steps(()))
         self.assertEqual(0, total_steps([]))
         self.assertEqual(3, total_steps([5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11]))
         self.assertEqual(0, total_steps([4, 5, 7, 7, 13]))
+        self.assertEqual(0, total_steps([3, 4]))
         self.assertEqual(1, total_steps([4, 3]))
         self.assertEqual(1, total_steps([4, 3, 5]))
         self.assertEqual(2, total_steps([4, 3, 3]))
@@ -40,5 +46,6 @@ class NonDescendingTest(unittest.TestCase):
         self.assertEqual(1, total_steps([4, 3, 14, 13]))
         self.assertEqual(2, total_steps([4, 3, 3, 14, 13]))
         self.assertEqual(3, total_steps([4, 3, 3, 3, 14, 13]))
-        self.assertEqual(0, total_steps(list(range(1, 1_000))))
-        self.assertEqual(999, total_steps(list(range(1_000, 0, -1))))
+        self.assertEqual(3, total_steps((4, 3, 3, 3, 14, 13)))
+        self.assertEqual(0, total_steps(range(1, 1_000)))
+        self.assertEqual(999, total_steps(range(1_000, 0, -1)))
