349cb6ebbdafc44c63b71ace3531545d1f64dbd7
John Hanley <jhanley708-git@s6.sector6.net>
2024-03-18 06:51:08 -0700

    Use graph-tool

--- geo/wed/so/spell_d/grimoire.py
+++ geo/wed/so/spell_d/grimoire.py
@@ -1,18 +1,27 @@
 #! /usr/bin/env python
 # Copyright 2024 John Hanley. MIT licensed.
-# from https://codereview.stackexchange.com/questions/291064/longest-spell-to-cast-from-pages-of-spellbook
+# from https://codereview.stackexchange.com/questions/291086/longest-spell-to-cast-from-pages-of-spellbook-follow-up
 import io
+
+from graph_tool import Graph
 
 
 class Grimoire:
     def __init__(self, in_file: io.TextIOWrapper):
+        """Deserialize a graph, from its specified contest format.
+
+        First line of text gives `n`, the number of nodes.
+        Then we see `n` distinct 1-origin node names, each in the range 1 through n.
+        """
         n = int(in_file.readline())
-        self.pages = [int(in_file.readline()) for _ in range(n)]
+        pairs = [(i, int(in_file.readline()) - 1) for i in range(n)]
+        self.g = Graph(g=pairs)
 
     def serialize(self) -> str:
-        lines = list(map(str, self.pages))
-        return "\n".join([f"{len(self.pages)}"] + lines) + "\n"
+        n = len(self.g)
+        lines = [str(1 + self.g.vertex_index[self.g.vertex(i)]) for i in range(n)]
+        return "\n".join([f"{n}"] + lines) + "\n"
 
     def longest_spell(self) -> int:
-        """Returns the longest spell that can be cast from the pages."""
-        return len(self.pages)
+        """Returns the longest spell that can be cast from the pages of the grimoire."""
+        return len(self.g)
