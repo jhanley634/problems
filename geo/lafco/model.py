1d3b2ed83eee581bc300157c589a8ae642536b71
John Hanley <jhanley708-git@s6.sector6.net>
2024-05-05 17:10:44 -0700

    Add owner table

--- geo/lafco/model.py
+++ geo/lafco/model.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # Copyright 2024 John Hanley. MIT licensed.
-from sqlalchemy import Text
+from sqlalchemy import Integer, Text
 from sqlalchemy.orm import declarative_base, mapped_column
 
 Base = declarative_base()
@@ -11,3 +11,18 @@ class ApnAddress(Base):  # type: ignore [misc, valid-type]
 
     apn = mapped_column(Text, primary_key=True)
     situs_addr = mapped_column(Text, index=True, nullable=False)
+
+
+class Owner(Base):  # type: ignore [misc, valid-type]
+    __tablename__ = "owner"
+
+    apn = mapped_column(Text, primary_key=True)
+    category = mapped_column(Text, nullable=False)
+    units = mapped_column(Integer, nullable=False)
+    first_owner = mapped_column(Text, index=True, nullable=False)
+    bus_name = mapped_column(Text)
+    location = mapped_column(Text)
+    address = mapped_column(Text, index=True, nullable=False)
+    city = mapped_column(Text)
+    st = mapped_column(Text)
+    zip = mapped_column(Text, nullable=False)
---
+++ geo/lafco/table_owner.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# Copyright 2024 John Hanley. MIT licensed.
+"""
+Datasource is the EPS093KO spreadsheet
+(from O'Connor Water?)
+"""
+
+from sqlalchemy import MetaData
+
+from geo.lafco.lafco_util import get_engine, get_session
+from geo.lafco.model import Owner
+from geo.lafco.parcel_owner import get_owner
+
+_drop_columns = [
+    "charge",
+    "rate",
+    "premis",
+    "prevwtr",
+    "water",
+    "prev",
+    "comment",
+    "tra",
+    "exemptions",
+    "care_of",
+    "mail_address",
+]
+
+
+def create_table_owner() -> None:
+    df = get_owner().drop(columns=_drop_columns)
+    df = df.drop_duplicates(subset=["apn"])  # discard ~ 50 dup rows
+    engine = get_engine()
+    metadata = MetaData()
+    metadata.create_all(engine, tables=[Owner.__table__])
+    with get_session() as sess:
+        sess.query(Owner).delete()
+        sess.commit()
+    print(df)
+    print(df.describe())
+    print(df.info())
+    df.to_sql("owner", engine, if_exists="append", index=False)
+
+
+if __name__ == "__main__":
+    create_table_owner()
