9c2292d75b31b911f546c1b816207cf5785ea2fb
John Hanley <jhanley708-git@s6.sector6.net>
2024-05-25 16:02:41 -0700

    Display the max CDF error

--- geo/lafco/so/lin_phi.py
+++ geo/lafco/so/lin_phi.py
@@ -16,11 +16,11 @@ def lin_phi(x: float, mu: float = 0, sigma: float = 1) -> float:
     if z >= 0:
         return 1 - 0.5 * math.exp(-0.72 * z - 0.42 * z**2)
     else:
-        return 0.5 * math.exp(-0.72 * abs(z) - 0.42 * z**2)
+        return 0.5 * math.exp(0.72 * z - 0.42 * z**2)
 
 
 def gen_df(n: int = 1_000) -> Generator[dict[str, float | str], None, None]:
-    for x in np.linspace(-2.0, 2.0, num=n):
+    for x in np.linspace(-3.0, 3.0, num=n):
         yield {"x": x, "value": lin_phi(x), "type": "cdf"}
         yield {"x": x, "value": lin_phi(x) - norm.cdf(x), "type": "error"}
 
@@ -29,3 +29,10 @@ if __name__ == "__main__":
     df = pd.DataFrame(gen_df())
     sns.scatterplot(data=df, x="x", y="value", hue="type")
     plt.show()
+
+    df1 = pd.DataFrame()
+    df1["x"] = np.unique(df.x)
+    df1["cdf"] = df1.x.apply(lin_phi)
+    df1["error"] = df1.cdf - norm.cdf(df1.x)
+    # display the max error
+    print(df1.describe())
