8b3a700bc3da65b764b8bab1ab46dd6252c6c34e
John Hanley <jhanley708-git@s6.sector6.net>
2024-06-01 05:42:33 -0700

    Initial lunar lander

---
+++ geo/gym/lander.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# Copyright 2024 John Hanley. MIT licensed.
+import gymnasium as gym
+
+
+def lander() -> None:
+    env = gym.make("LunarLander-v2", render_mode="human")
+    observation, info = env.reset(seed=42)
+    for _ in range(1000):
+        action = env.action_space.sample()  # this is where you would insert your policy
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+
+
+if __name__ == "__main__":
+    lander()
