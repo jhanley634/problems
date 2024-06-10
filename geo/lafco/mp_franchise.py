f827ca45bb4a8b6aff0837a6ae15eab242091d83
John Hanley <jhanley708-git@s6.sector6.net>
2024-06-10 15:03:39 -0700

    Produce a map of disenfranchised citizens, for Drew

--- geo/lafco/mp_franchise.py
+++ geo/lafco/mp_franchise.py
@@ -1,10 +1,11 @@
-#! /usr/bin/env python
+#! /usr/bin/env streamlit run --server.runOnSave true
 # Copyright 2024 John Hanley. MIT licensed.
 from collections.abc import Generator
 from pathlib import Path
 
 from sqlalchemy.orm import Session
 import pandas as pd
+import streamlit as st
 
 from geo.lafco.geocode import Geocoder
 from geo.lafco.lafco_util import LAFCO_DIR, clean_column_names, get_session
@@ -31,21 +32,27 @@ def _street_num(addr: str) -> str:
     return f"{w1} {w2} {n:06d}"
 
 
+GREEN = (0, 255, 0, 255)
+
+
 def menlo_park_disenfranchised(sess: Session) -> Generator[dict[str, str], None, None]:
     g = Geocoder()
-    df = _get_df()
-    for _, row in df.iterrows():
+    for _, row in _get_df().iterrows():
         own = sess.query(Owner).filter(Owner.apn == row.apn).one_or_none()
         assert own.address == row.epa_address
         addr = f"{own.address}, {own.city} {own.st} {own.zip}".replace("'", "")
         if own.city == "MENLO PARK" and own.st == "CA":
             loc = g.get_location(addr)
             assert addr == loc.addr, (own, loc)
+            if loc.lat > 37.464 or loc.lon > -122.139:
+                continue
             yield {
                 "apn": own.apn,
                 "first_owner": own.first_owner.ljust(30),
                 "addr": own.address.replace("'", ""),
                 "street_num": _street_num(own.address),
+                "lat": loc.lat,
+                "lon": loc.lon,
             }
 
 
@@ -54,6 +61,11 @@ if __name__ == "__main__":
         df = pd.DataFrame(menlo_park_disenfranchised(sess))
         df = df.sort_values(by=["street_num", "first_owner", "apn"])
         df = df.drop(columns=["street_num"])
-        out_file = Path("/tmp/menlo_park_disenfranchised.csv")
+        print(df)
+        print(df.describe())
+        out_file = Path("/tmp/menlo_park_disenfranchised_protesters.csv")
         df.to_csv(out_file, index=False)
         print(out_file)
+        st.title("EPASD landowners that signed")
+        st.markdown("----")
+        st.map(df, size=4, color=GREEN)
