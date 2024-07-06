338a4d6838dd1b0a6b7fe26d98b75d2e577d7e87
John Hanley <jhanley708-git@s6.sector6.net>
2024-07-06 09:57:16 -0700

    Add iterative_fibo

---
+++ geo/lafco/so/iterative_fibo.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# Copyright 2024 John Hanley. MIT licensed.
+from functools import cache
+
+from linetimer import CodeTimer
+
+
+def iterative_fibo(n: int) -> int:
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+
+@cache
+def recursive_fibo(n: int) -> int:
+    if n < 2:
+        return n
+    return recursive_fibo(n - 1) + recursive_fibo(n - 2)
+
+
+def binet_fibo(n: int) -> int:
+    phi = (1 + 5**0.5) / 2
+    return int((phi**n - (-phi) ** -n) / 5**0.5)
+
+
+if __name__ == "__main__":
+
+    answer = 280571172992510140037611932413038677189525
+
+    with CodeTimer("iterative"):
+        assert answer == iterative_fibo(200)
+
+    with CodeTimer("recursive"):
+        assert answer == recursive_fibo(200)
+
+    # Alas, FP is no match for BigInt.
+    for n in range(72):
+        assert binet_fibo(n) == iterative_fibo(n)
+    for n in range(86):
+        assert binet_fibo(n) - iterative_fibo(n) < 1000
+    n = 72
+    assert binet_fibo(n) == iterative_fibo(n) + 1
+    n -= 1
+    with CodeTimer("analytic "):
+        assert binet_fibo(n) == iterative_fibo(n)
