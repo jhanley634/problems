ed068e5ff447cc913ce4839a1dc62afa0d3d6ffd
John Hanley <jhanley708-git@s6.sector6.net>
2024-07-16 16:57:58 -0700

    Add CachedLimiterSession

--- stock/yf/closing_price.py
+++ stock/yf/closing_price.py
@@ -1,12 +1,29 @@
 #! /usr/bin/env python
 # Copyright 2024 John Hanley. MIT licensed.
+from pyrate_limiter import Duration, Limiter, RequestRate
+from requests import Session
+from requests_cache import CacheMixin, SQLiteCache
+from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
 import typer
 import yfinance as yf
 
 
+class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
+    pass
+
+
+session = CachedLimiterSession(
+    limiter=Limiter(
+        RequestRate(2, Duration.SECOND * 5)
+    ),  # max 2 requests per 5 seconds
+    bucket_class=MemoryQueueBucket,
+    backend=SQLiteCache("/tmp/yfinance.cache"),
+)
+
+
 def stock_report(symbols: str = "ADBE,MSFT") -> None:
     first, *_ = symbols.upper().split(",")
-    tickers = yf.Tickers(symbols)
+    tickers = yf.Tickers(symbols, session=session)
     tickers.history(period="1mo", progress=False)
 
     hist_df = tickers.history(period="1mo")
