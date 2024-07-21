4efd3c0998003e8c71221e60cb40d20d045dbb3d
John Hanley <jhanley708-git@s6.sector6.net>
2024-07-21 11:10:19 -0700

    Add _get_geocoded_df

---
+++ geo/lafco/street/street_counts.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# Copyright 2024 John Hanley. MIT licensed.
+from collections import Counter
+from collections.abc import Generator
+from pprint import pp
+import io
+
+import pandas as pd
+
+from geo.gpx.so.word_publisher import get_document
+
+
+def _get_geocoded_df() -> pd.DataFrame:
+    url = (
+        "https://raw.githubusercontent.com/jhanley634/dojo-2024-06-18-geocode/main"
+        "/geocoding/data/geocoded.csv"
+    )
+    text = io.StringIO(get_document(url))
+    df = pd.read_csv(text)
+    cols = "address city st zip housenum  street lat lon".split()
+    assert cols == df.columns.tolist(), df.columns
+    return df
+
+
+def _get_streets() -> Generator[str, None, None]:
+    for row in _get_geocoded_df().itertuples():
+        s = f"{row.zip} {row.street}"
+        yield s
+
+
+def report() -> None:
+    c = Counter(_get_streets())
+    print(c)
+    pp(c.most_common(500))
+
+
+if __name__ == "__main__":
+    report()
