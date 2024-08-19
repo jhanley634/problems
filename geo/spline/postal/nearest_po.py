3577f147abeea1026572343176dae7133b402d21
John Hanley <jhanley708-git@s6.sector6.net>
2024-08-18 17:16:48 -0700

    Add nearest_post_office

--- geo/spline/postal/nearest_po.py
+++ geo/spline/postal/nearest_po.py
@@ -1,4 +1,15 @@
 #! /usr/bin/env python
 # Copyright 2024 John Hanley. MIT licensed.
+from sqlalchemy import select
+from uszipcode import SearchEngine
+from uszipcode import SimpleZipcode as Zip
 
-from uszipcode import SearchEngine, SimpleZipcode
+
+def nearest_post_office() -> None:
+    query = select(Zip).where(Zip.zipcode == "10001")
+    results = SearchEngine().ses.scalar(query)
+    print(results)
+
+
+if __name__ == "__main__":
+    nearest_post_office()
