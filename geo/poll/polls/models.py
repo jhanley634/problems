5cc4101b5ddfa86c84d967f6fd30a650a8520f08
John Hanley <jhanley708-git@s6.sector6.net>
2024-08-24 13:17:56 -0700

    Add was_published_recently()

--- geo/poll/polls/models.py
+++ geo/poll/polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.db import models
 from django.db.models import (
     CASCADE,
     CharField,
@@ -6,14 +9,24 @@ from django.db.models import (
     IntegerField,
     Model,
 )
+from django.utils import timezone
 
 
 class Question(Model):
     question_text = CharField(max_length=200)
     pub_date = DateTimeField("date published")
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(Model):
     question = ForeignKey(Question, on_delete=CASCADE)
     choice_text = CharField(max_length=200)
     votes = IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
