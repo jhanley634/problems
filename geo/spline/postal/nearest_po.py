cb2a300f70c7b465ac3ed74c8314f4f85ff5b92e
John Hanley <jhanley708-git@s6.sector6.net>
2024-08-26 17:17:33 -0700

    Don't use text(...)

--- geo/spline/postal/nearest_po.py
+++ geo/spline/postal/nearest_po.py
@@ -1,32 +1,25 @@
 #! /usr/bin/env python
 # Copyright 2024 John Hanley. MIT licensed.
-from pprint import pp
+from pathlib import PosixPath
 
-from sqlalchemy import text
 from sqlalchemy.orm import aliased
 from uszipcode import SearchEngine
 from uszipcode import SimpleZipcode as Zip
 
 
 def nearest_post_office() -> None:
-    # z1 = aliased(Zip)
-    # z2 = aliased
-    select = """
-    SELECT z1.zipcode, z2.zipcode, z1.major_city, z1.state
-    FROM simple_zipcode AS z1
-    JOIN simple_zipcode AS z2
-    ON z1.zipcode < z2.zipcode
-    WHERE z2.zipcode <= '01009'
-    AND z1.state not in ('PR', 'VI')
-    AND z2.state not in ('PR', 'VI')
-    """
+    db_file = f"{SearchEngine().db_file_path}"
+    assert f"{PosixPath("~/.uszipcode/simple_db.sqlite").expanduser()}" == db_file
+
+    z1 = aliased(Zip)
+    z2 = aliased(Zip)
     query = (
-        SearchEngine().ses.execute(text(select))
-        # .join(z2, z2.zipcode <= "10010")
-        # .where(z1.zipcode < z2.zipcode)
-        # .where(z2.zipcode <= "10010")
+        SearchEngine()
+        .ses.query(z1.zipcode, z2.zipcode, z1.major_city, z1.state)
+        .where(z1.zipcode < z2.zipcode)
+        .where(z2.zipcode <= "01009")
     )
-    print(text(select))
+    print(query)
     for result in query.all():
         print(result._asdict())
 
