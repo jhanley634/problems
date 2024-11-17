dd105573113510b142a777049da65bf7c323a6b5
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-17 11:34:44 -0800

    Add roundtrip_to_disk

--- geo/spline/so/finding_breaks/generate_sorted_input.py
+++ geo/spline/so/finding_breaks/generate_sorted_input.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 # from https://stackoverflow.com/questions/79195892/most-efficient-way-to-get-unique-elements-from-sorted-list
-
+from pathlib import Path
 from random import choices
 
 import numpy as np
 import numpy.typing as npt
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 
 def generate_sorted_values(
@@ -25,9 +27,25 @@ def generate_sorted_numpy_array(
     population = range(distinct_values)
     rng = np.random.default_rng()
     xs = rng.choice(population, n)
-    assert isinstance(xs, np.ndarray)
     return np.sort(xs)
+
+
+def roundtrip_to_disk(temp_file: Path = Path("/tmp/sorted_xs.parquet")) -> None:
+    xs = generate_sorted_numpy_array()
+    assert (1_000_000,) == xs.shape
+
+    table = pa.table({"x": xs})
+    pq.write_table(table, temp_file)
+
+    # Now read it back in.
+    table2 = pq.read_table(temp_file)
+    xs2 = np.array(table2["x"])
+    assert xs.shape == xs2.shape
+    assert (xs == xs2).all()
+    assert 675 == temp_file.stat().st_size  # It compresses quite nicely.
+    # temp_file.unlink()
 
 
 if __name__ == "__main__":
     print(generate_sorted_numpy_array(24))
+    roundtrip_to_disk()
