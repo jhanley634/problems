4336779a8587505c16bb8ce2d83c02586cf536a3
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-17 11:09:54 -0800

    Add generate_sorted_numpy_array

--- geo/spline/so/finding_breaks/generate_sorted_input.py
+++ geo/spline/so/finding_breaks/generate_sorted_input.py
@@ -1,16 +1,33 @@
 #! /usr/bin/env python
 # from https://stackoverflow.com/questions/79195892/most-efficient-way-to-get-unique-elements-from-sorted-list
 
-
 from random import choices
 
+import numpy as np
+import numpy.typing as npt
 
-def generate_sorted_values(n: int = 1_000_000, distinct_values: int = 12) -> list[int]:
+
+def generate_sorted_values(
+    n: int = 1_000_000,
+    distinct_values: int = 12,
+) -> list[int]:
     """Returns a sorted list of random non-negative integers."""
     population = range(distinct_values)
     xs = choices(population, k=n)
     return sorted(xs)
 
 
+def generate_sorted_numpy_array(
+    n: int = 1_000_000,
+    distinct_values: int = 12,
+) -> npt.NDArray[np.int_]:
+    """Returns a sorted array of random non-negative integers."""
+    population = range(distinct_values)
+    rng = np.random.default_rng()
+    xs = rng.choice(population, n)
+    assert isinstance(xs, np.ndarray)
+    return np.sort(xs)
+
+
 if __name__ == "__main__":
-    print(generate_sorted_values(40))
+    print(generate_sorted_numpy_array(24))
