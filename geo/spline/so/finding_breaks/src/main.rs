dd734ac026b84e8cfed7514aaae3802146586234
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-17 21:15:05 -0800

    Add find_break

--- geo/spline/so/finding_breaks/src/main.rs
+++ geo/spline/so/finding_breaks/src/main.rs
@@ -26,6 +26,28 @@ fn sum_int16(xs: &[i16]) -> i64 {
     xs.iter().map(|&x| x as i64).sum()
 }
 
+/// Finds an xs index where the element values change.
+fn find_break(xs: &[i16], start: usize, end: usize) -> usize {
+    assert!(!xs.is_empty());
+    let mut i = start;
+    let mut j = end;
+    assert!(start < end);
+    assert!(i < j);
+
+    while i < j {
+        let mid = i + (j - i) / 2;
+        if xs[i] == xs[mid] {
+            i = mid + 1;
+        } else {
+            j = mid;
+        }
+    }
+    assert!(i == 0 || xs[i - 1] != xs[i]);
+    println!("at i = {}:\t{},\t{}", i, xs[i - 1], xs[i]);
+
+    i
+}
+
 #[macro_use]
 extern crate timeit;
 
@@ -41,6 +63,9 @@ fn main() -> Result<(), Box<dyn Error>> {
             assert!(s == million_sum || s == ten_million_sum);
         });
         println!("sum_int16() took {:.4} s", sec);
+
+        let br = find_break(&xs, 0, xs.len());
+        println!("Break at {}", br);
     });
     Ok(())
 }
