9d57691eb050c02dc241adc9969f8f816a25b192
John Hanley <jhanley708-git@s6.sector6.net>
2024-12-21 10:29:50 -0800

    Do five episodes

--- geo/gymnasium/cart_pole.py
+++ geo/gymnasium/cart_pole.py
@@ -1,20 +1,26 @@
 #! /usr/bin/env python
+# Copyright 2024 John Hanley. MIT licensed.
+
+from time import sleep
 
 import gymnasium as gym
 import numpy as np
 
 
-def main() -> None:
+def main(num_episodes: int = 5) -> None:
     env = gym.make("CartPole-v1", render_mode="human")  # or render_mode="rgb_array"
-    env.reset(seed=42, options={"low": -0.1, "high": 0.1})
-    term = False
 
-    while not term:
-        env.render()
-        obs, reward, term, _trunc, _info = env.step(env.action_space.sample())
-        print(reward, np.array(obs))
+    for _ in range(num_episodes):
 
-    env.reset()
+        env.reset(options={"low": -0.1, "high": 0.1})
+        term = False
+        while not term:
+            env.render()
+            obs, reward, term, _trunc, _info = env.step(env.action_space.sample())
+            print(reward, np.array(obs))
+
+        sleep(1.0)
+
     env.close()
 
 
---
+++ geo/gymnasium/cart_pole_learner.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python
+# Copyright 2024 John Hanley. MIT licensed.
+# Q-learner, from https://towardsdatascience.com/deep-q-learning-for-the-cartpole-44d761085c2f
