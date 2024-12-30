9f1e580276c579f4d71f30794b1523202165d1e9
John Hanley <jhanley634@users.noreply.github.com>
2024-12-29 16:56:11 -0800

    Add only_text_files.py

---
+++ geo/spline/only_text_files.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# Copyright 2024 John Hanley. MIT licensed.
+
+# usage:
+#    find . | sort | only_text_files.py | xargs grep ...
+
+from pathlib import Path
+from pprint import pp
+import os
+import re
+import sys
+
+import magic
+
+
+def filter_path_names() -> None:
+
+    exclude_re = re.compile(r"/\.git/|/\.venv/|/__pycache__/")
+
+    for line in sys.stdin:
+        if exclude_re.search(line):
+            continue
+        file = Path(line.rstrip())
+        if file.suffix == '.json' or not file.is_file():
+            continue
+
+        if " text" in magic.from_file(f"{file}"):
+            print(file)
+
+
+if __name__ == "__main__":
+    filter_path_names()
