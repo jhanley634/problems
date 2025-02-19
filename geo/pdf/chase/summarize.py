26750b5ef9beba6f8f716067efef8974f4b5d01a
John Hanley <jhanley634@users.noreply.github.com>
2025-02-19 12:45:25 -0800

    Read three lines at a time

--- geo/pdf/chase/summarize.py
+++ geo/pdf/chase/summarize.py
@@ -1,14 +1,33 @@
 #! /usr/bin/env python
 # Copyright 2025 John Hanley. MIT licensed.
 
+from collections.abc import Generator
 from pathlib import Path
 
 from pdftotext import PDF
 
 
-def report(fin: Path) -> None:
-    for page in PDF(fin):
-        print(type(page), page)
+def get_lines(file: Path) -> Generator[str]:
+    preamble = True
+    with open(file, "rb") as fin:
+        for page in PDF(fin):
+            for line in page.splitlines():
+                if line and not preamble:
+                    yield line
+                if line.startswith("AUTOMATIC PAYMENT - THANK YOU"):
+                    preamble = False
+
+
+def report(file: Path, year: int = 2024) -> None:
+    gl = get_lines(file)
+    next(gl)
+    line = next(gl)
+    assert "PURCHASE" == line, line
+    while True:
+        day = next(gl)
+        vendor = next(gl)
+        amt = next(gl)
+        print(day, amt, "\t", vendor)
 
 
 def main() -> None:
@@ -17,8 +36,7 @@ def main() -> None:
     nnn = "0??"
     glob = f"????-202?{yymm}-Statement-{nnn}.pdf"
     for file in sorted(desktop.glob(glob)):
-        with open(file, "rb") as fin:
-            report(fin)
+        report(file)
 
 
 if __name__ == "__main__":
