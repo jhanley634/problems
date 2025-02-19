542b0881ee722f7f1f9e840c4e8af6078fa422d8
John Hanley <jhanley634@users.noreply.github.com>
2025-02-19 13:12:47 -0800

    Filter against valid strptime() values

--- geo/pdf/chase/summarize.py
+++ geo/pdf/chase/summarize.py
@@ -2,32 +2,39 @@
 # Copyright 2025 John Hanley. MIT licensed.
 
 from collections.abc import Generator
+from datetime import UTC
 from pathlib import Path
+import datetime as dt
 
 from pdftotext import PDF
 
 
 def get_lines(file: Path) -> Generator[str]:
-    preamble = True
+    in_preamble = True
     with open(file, "rb") as fin:
         for page in PDF(fin):
             for line in page.splitlines():
-                if line and not preamble:
+                if line and not in_preamble:
                     yield line
                 if line.startswith("AUTOMATIC PAYMENT - THANK YOU"):
-                    preamble = False
+                    in_preamble = False
 
 
 def report(file: Path, year: int = 2024) -> None:
+    day = dt.datetime.now(UTC)
+    vendor = ""
+    amt = 0.0
     gl = get_lines(file)
-    next(gl)
-    line = next(gl)
-    assert "PURCHASE" == line, line
-    while True:
-        day = next(gl)
-        vendor = next(gl)
-        amt = next(gl)
-        print(day, amt, "\t", vendor)
+    for _ in gl:
+        mm_dd = next(gl)
+        try:
+            day = dt.datetime.strptime(f"{year}/{mm_dd} +0000", "%Y/%m/%d %z")
+            vendor = next(gl)
+            amt = float(next(gl))
+        except ValueError:
+            pass
+        if amt:
+            print(day.date(), f"{amt:9.2f}\t", vendor)
 
 
 def main() -> None:
