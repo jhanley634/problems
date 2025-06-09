a9809714b18593ed487d43d1a187b50771dc07a0
John Hanley <jhanley634@users.noreply.github.com>
2025-06-09 12:28:17 -0700

    Add _summarize_headers()

--- mail/read_demo.py
+++ mail/read_demo.py
@@ -1,26 +1,63 @@
 #! /usr/bin/env python
 
-import imaplib
+
+from email.message import Message
+from imaplib import IMAP4_SSL
+import email
 import os
 
 
-def read_recent_email() -> None:
+def _get_imap_client() -> IMAP4_SSL:
     server = os.getenv("GMAIL_IMAP_SERVER", "imap.gmail.com")
     user = os.getenv("GMAIL_USER", "jhanley741@gmail.com")
     pw = os.environ["GMAIL_PASSWORD"]
-    client = imaplib.IMAP4_SSL(server)
+    client = IMAP4_SSL(server)
     client.login(user, pw)
     client.noop()
+    return client
 
-    status, payload = client.select("INBOX")
+
+def _select_inbox(client: IMAP4_SSL, inbox_name: str = "INBOX") -> IMAP4_SSL:
+    status, data = client.select(inbox_name)
     assert "OK" == status, status
-    payload0: bytes = payload[0] or b""
-    num_msgs = int(payload0.decode())
-    print(num_msgs)
+    data0 = data[0] or b""
+    num_msgs = int(data0.decode())
+    assert num_msgs > 0, num_msgs
+    return client
 
-    client.close()
-    client.logout()
+
+def display_unread_messages(client: IMAP4_SSL) -> None:
+
+    try:
+        status, data = client.search(None, "UNSEEN")
+        assert "OK" == status, status
+        # print(data)  # e.g. [b'10138 10139']
+        unseen_msg_uids = data[0].split()
+        print(f"Found {len(unseen_msg_uids)} unread\n")
+
+        for msg_uid in unseen_msg_uids:
+            status, data = client.fetch(msg_uid, "(RFC822.HEADER)")
+            assert "OK" == status, status
+            pair, _ = data
+            _, hdrs = pair or (b"", b"")
+            assert isinstance(hdrs, bytes), hdrs
+            msg = email.message_from_bytes(hdrs)
+            print(_summarize_headers(msg))
+
+    finally:
+        client.close()
+        client.logout()
+
+
+def _summarize_headers(msg: Message) -> str:
+    subject = msg.get("Subject", "No Subject")
+    from_ = msg.get("From", "Unknown Sender")
+    date = msg.get("Date", "No Date")
+
+    return f"""From: {from_}
+Subject: {subject}
+Date: {date}\n"""
 
 
 if __name__ == "__main__":
-    read_recent_email()
+    display_unread_messages(_select_inbox(_get_imap_client()))
