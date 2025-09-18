8544284300bfb0c0a15c55d0c288dc8e3121eff6
John Hanley <jhanley634@users.noreply.github.com>
2025-09-17 23:44:07 -0700

    Prefer a "with" context handler to issue env.close()

--- geo/gymnasium/cart_pole.py
+++ geo/gymnasium/cart_pole.py
@@ -8,20 +8,17 @@ import numpy as np
 
 
 def main(num_episodes: int = 5) -> None:
-    env = gym.make("CartPole-v1", render_mode="human")  # or render_mode="rgb_array"
+    # or                         render_mode="rgb_array"
+    with gym.make("CartPole-v1", render_mode="human") as env:
+        for _ in range(num_episodes):
+            env.reset(options={"low": -0.1, "high": 0.1})
+            term = False
+            while not term:
+                env.render()
+                obs, reward, term, _trunc, _info = env.step(env.action_space.sample())
+                print(reward, np.array(obs))
 
-    for _ in range(num_episodes):
-
-        env.reset(options={"low": -0.1, "high": 0.1})
-        term = False
-        while not term:
-            env.render()
-            obs, reward, term, _trunc, _info = env.step(env.action_space.sample())
-            print(reward, np.array(obs))
-
-        sleep(1.0)
-
-    env.close()
+            sleep(1.0)
 
 
 if __name__ == "__main__":
--- geo/gymnasium/lander.py
+++ geo/gymnasium/lander.py
@@ -4,16 +4,16 @@ import gymnasium as gym
 
 
 def lander() -> None:
-    env = gym.make("LunarLander-v2", render_mode="human")
-    _observation, _info = env.reset(seed=42)
-    for _ in range(1000):
-        action = env.action_space.sample()  # this is where you would insert your policy
-        _observation, _reward, terminated, truncated, _info = env.step(action)
+    with gym.make("LunarLander-v2", render_mode="human") as env:
+        _observation, _info = env.reset(seed=42)
+        for _ in range(1000):
+            action = (
+                env.action_space.sample()
+            )  # this is where you would insert your policy
+            _observation, _reward, terminated, truncated, _info = env.step(action)
 
-        if terminated or truncated:
-            _observation, _info = env.reset()
-
-    env.close()
+            if terminated or truncated:
+                _observation, _info = env.reset()
 
 
 if __name__ == "__main__":
